d429bf5d4999b81e1991a48596d2b5efeb4299fd
Shawn Melton <wshawnmelton@gmail.com>
2020-06-28 23:17:01 -0500

    Initial commit

---
+++ SecretServer.psd1
@@ -0,0 +1,21 @@
+@{
+    ModuleVersion = '0.0.1'
+    RootModule = 'SecretServer.psm1'
+    Description = 'PowerShell Tools for Thycotic Secret Server'
+    Guid = 'e6b56c5f-41ac-4ba4-8b88-2c063f683176'
+    PrivateData = @{
+        PSData = @{
+            Tags = 'SecretServer', 'Thycotic', 'DevOps'
+            ProjectURI = 'https://thycotic.com/products/secret-server/'
+            LicenseURI = ''
+            ReleaseNotes = @'
+0.1    :
+---
+Initial Commit
+'@
+        }
+    }
+    Author = 'Shawn Melton'
+    Copyright = '2020 Thycotic Secret Server'
+    PowerShellVersion ='5.1'
+}
---
+++ SecretServer.psm1
@@ -0,0 +1,49 @@
+#region Import Functions
+foreach ($file in Get-ChildItem -Path $psScriptRoot\functions -Filter *-*.ps1) {
+    . $file.FullName
+}
+#endregion Import Functions
+
+Add-Type -AssemblyName System.Web # Add System.Web now, in the unlikely event it was not already loaded.
+
+# Load session object for token authentication
+
+$TssSession = [ordered]@{
+    SecretServerHost = $null
+    ApiVersion       = "api/v1"
+    AuthToken        = $null
+    RefreshToken     = $null
+    RefreshCount     = $null
+    StartTime        = $null
+    ExpiresInSec     = $null
+    TimeOfDeath      = $null
+    AutoReconnect    = $false
+}
+New-Variable -Name TssSession -Value $TssSession -Scope Script -Force
+
+#region Import Parts
+
+# Parts are simple .ps1 files beneath a /Parts directory that can be used throughout the module.
+$partsDirectory = $( # Because we want to be case-insensitive, and because it's fast
+    foreach ($dir in [IO.Directory]::GetDirectories($psScriptRoot)) {
+        # [IO.Directory]::GetDirectories()
+        if ($dir -imatch "\$([IO.Path]::DirectorySeparatorChar)Parts$") {
+            # and some Regex
+            [IO.DirectoryInfo]$dir;break # to find our parts directory.
+        }
+    })
+
+if ($partsDirectory) {
+    # If we have parts directory
+    foreach ($partFile in $partsDirectory.EnumerateFileSystemInfos()) {
+        # enumerate all of the files.
+        if ($partFile.Extension -ne '.ps1') { continue } # Skip anything that's not a PowerShell script.
+        $partName = # Get the name of the script.
+        $partFile.Name.Substring(0, $partFile.Name.Length - $partFile.Extension.Length)
+        $ExecutionContext.SessionState.PSVariable.Set( # Set a variable
+            $partName, # named the script that points to the script (e.g. $foo = gcm .\Parts\foo.ps1)
+            $ExecutionContext.SessionState.InvokeCommand.GetCommand($partFile.Fullname, 'ExternalScript')
+        )
+    }
+}
+#endregion Import Parts
