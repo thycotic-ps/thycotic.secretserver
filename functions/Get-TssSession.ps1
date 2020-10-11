88611ec74c6d99efd27098a01261d8291e5cd232
Shawn Melton <wshawnmelton@gmail.com>
2020-10-11 16:55:27 -0500

    Get-TssSession - adding cbh

--- functions/Get-TssSession.ps1
+++ functions/Get-TssSession.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+Retrieve the internal TssSession Object
+
+.DESCRIPTION
+Retrieve the internal TssSession Object
+
+.EXAMPLE
+PS C:\> Get-TssSession
+
+Outputs the current session properties
+
+.OUTPUTS
+System.Management.Automation.PSCustomObject
+#>
 function Get-TssSession {
     [cmdletbinding()]
     param()
