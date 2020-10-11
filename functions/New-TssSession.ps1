b72e572decb2e74866e676bc44461709cd03c19e
Shawn Melton <wshawnmelton@gmail.com>
2020-10-11 16:55:11 -0500

    New-TssSession - adding cbh

--- functions/New-TssSession.ps1
+++ functions/New-TssSession.ps1
@@ -1,7 +1,51 @@
+<#
+.SYNOPSIS
+Create new session
+
+.DESCRIPTION
+Create a new TssSession for working with a Secret Server
+
+.PARAMETER SecretServer
+Secret Server URL
+
+.PARAMETER Credential
+Secret Server account to be used for authentication
+
+.PARAMETER UseRefreshToken
+Use the refresh token for reauthenticating
+(to-do item)
+
+.PARAMETER RefreshLimit
+Use to specify the limit the refresh token can be used
+(to-do item)
+
+.PARAMETER AutoReconnect
+Use to have automatically reauthenticate if session timeout is hit
+(to-do item)
+
+.PARAMETER Raw
+Output raw response from the oauth2/token endpoint
+Internal TssSession object **is not** utilized
+
+.EXAMPLE
+PS C:\> $cred = [PSCredential]::new('apiuser',(ConvertTo-SecureString -String "Fancy%$#Passwod" -AsPlainText -Force))
+PS C:\> New-TssSession -SecretServer https://ssvault.com/SecretServer -Credential $cred
+
+A PSCredential is created for the apiuser account. The internal TssSession is updated upon successful authentication, and then output to the console.
+
+.EXAMPLE
+PS C:\> New-TssSession -SecretServer https://ssvault.com/SecretServer -Credential (Get-Credential apiuser) -Raw
+
+A prompt to ener the password for the apiuser is given by PowerShell. Upon successful authentication the response from the oauth2/token endpoint is output to the console.
+
+.OUTPUTS
+System.Management.Automation.PSCustomObject.
+#>
 function New-TssSession {
     [cmdletbinding(SupportsShouldProcess)]
     param(
         [Parameter(ParameterSetName = 'New')]
+        [Parameter(ParameterSetName = 'tss')]
         [Alias('Server')]
         [uri]
         $SecretServer,
@@ -11,6 +55,10 @@ function New-TssSession {
         [PSCredential]
         [Management.Automation.CredentialAttribute()]
         $Credential,
+
+        # Specify Access Token
+        [Parameter(ParameterSetName = 'tss')]
+        $AccessToken,
 
         # Utilize Refresh Token in TssSession to re-authenticate
         [Parameter(ParameterSetName = 'Refresh')]
@@ -43,7 +91,7 @@ function New-TssSession {
     }
 
     process {
-        if ($newTssParams.Contains('SecretServer') -and $newTssParams.Contains('Credential')) {
+        if ($newTssParams.Contains('SecretServer')) {
             $TssSession.SecretServerUrl = $SecretServer
         }
 
@@ -71,7 +119,9 @@ function New-TssSession {
         if (-not $PSCmdlet.ShouldProcess("POST $uri")) { return }
         $response = Invoke-TssRestApi @invokeParams
 
-        if ($response.access_token) {
+        if ($response.access_token -and $Raw) {
+            return $response
+        } else {
             $TssSession.AccessToken = $response.access_token
             $TssSession.RefreshToken = $response.refresh_token
             $TssSession.RefreshCount = if ($postContent.grant_type -eq 'refresh_token') { $TssSession.RefreshCount - 1 } else { $RefreshLimit }
@@ -79,10 +129,7 @@ function New-TssSession {
             $TssSession.StartTime = [datetime]::UtcNow
             $TssSession.TimeOfDeath = [datetime]::UtcNow.Add([timespan]::FromSeconds($response.expires_in))
             $TssSession.AutoReconnect = $AutoReconnect
-        }
-        if ($response.access_token -and $Raw) {
-            return $response
-        } else {
+
             Get-TssSession
         }
     }
