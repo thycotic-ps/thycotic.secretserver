365904e256f7f58bb6d6f26fecd0d2c969c9faee
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2020-12-05 18:08:37 -0600

    Get-TssSession - Expand test

--- tests/Get-TssSession.Tests.ps1
+++ tests/Get-TssSession.Tests.ps1
@@ -16,3 +16,30 @@ Describe "$commandName Unit Tests" {
         }
     }
 }
+
+Describe "$commandName returns the object" {
+    Context "Oauth2 authentication" {
+        BeforeEach {
+            $expiresIn = 1199
+            [uri]$secretServer = 'https://alpha'
+            $apiV = 'api/v1'
+            Mock Invoke-RestMethod {
+                @{
+                    access_token = [System.Convert]::ToBase64String((New-Guid).ToByteArray())
+                    token_type = 'bearer'
+                    expires_in = $expiresIn
+                    refresh_token = [System.Convert]::ToBase64String((New-Guid).ToByteArray())
+                }
+            }
+            $cred = [pscredential]::new('user',(ConvertTo-SecureString "p" -AsPlainText -Force))
+            New-TssSession -SecretServer $secretServer -Credential $cred > $null
+            $session = Get-TssSession
+        }
+        It "Is PSCustomObject type" {
+            $session.GetType().Name | Should -Be 'PSCustomObject'
+        }
+        It "Should have AccessToken as minimum (object testing is in test for New-TssSession)" {
+            $session.AccessToken | Should -Not -BeNullOrEmpty
+        }
+    }
+}
