fd82175a29b2d49b72a67dc040113d1ca6e900cc
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2020-12-10 23:45:53 -0600

    Get-TssSecret - add TssSession, adjust output

--- src/functions/Get-TssSecret.ps1
+++ src/functions/Get-TssSecret.ps1
@@ -6,6 +6,9 @@
     .DESCRIPTION
     Get a secret(s) from Secret Server
 
+    .PARAMETER TssSession
+    TssSession object created by New-TssSession
+
     .PARAMETER Id
     Secret ID to retrieve, accepts an array of IDs
 
@@ -16,22 +19,28 @@
     Output the raw response from the REST API endpoint
 
     .EXAMPLE
-    PS C:\> Get-TssSecret -Id 93
+    PS C:\> $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    PS C:\> Get-TssSecret -TssSession $session -Id 93
 
     Returns secret associated with the Secret ID, 93
 
     .EXAMPLE
-    PS C:\> Get-TssSecret -Id 1723 -Comment "Accessing application Y"
+    PS C:\> $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    PS C:\> Get-TssSecret -TssSession $session -Id 1723 -Comment "Accessing application Y"
 
     Returns secret associated with the Secret ID, 1723, providing required comment
 
     .NOTES
-    Requires New-TssSession session be set
+    Requires TssSession object returned by New-TssSession
     #>
     [cmdletbinding()]
     param(
+        # TssSession object passed for auth info
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [TssSession]$TssSession,
+
         # Return only specific Secret, Secret Id
-        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName="norm")]
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName,ParameterSetName = "norm")]
         [Alias("SecretId")]
         [int[]]
         $Id,
@@ -45,95 +54,107 @@
         $Raw
     )
     begin {
+        $tssParams = . $GetParams $PSBoundParameters 'Get-TssSecret'
         $invokeParams = @{ }
     }
 
     process {
-        . $TestTssSession -Session
-
-        foreach ($secret in $Id) {
-            $restResponse = $null
-            $errorResponse = $null
-            $uri = $TssSession.SecretServerUrl, $TssSession.ApiVersion, "secrets", $secret.ToString() -join '/'
-            if ($Comment) {
-                $uri = $uri, "restricted" -join "/"
-                $body = "{'comment':'$Comment', 'includeInactive':'$true'}"
-                $invokeParams.Uri = $Uri
-                $invokeParams.Method = 'POST'
-                $invokeParams.Body = $body
-            } else {
-                $uri = $uri, "includeInactive=true" -join "?"
-                $invokeParams.Uri = $Uri
-                $invokeParams.Method = 'GET'
-            }
-
-            $invokeParams.PersonalAccessToken = $TssSession.AccessToken
-            try {
-                $restResponse = Invoke-TssRestApi @invokeParams -ErrorAction Stop
-            } catch {
-                $errorResponse = $_.ErrorDetails.Message | ConvertFrom-Json
-            }
-
-            if ($Raw -and $restResponse) {
-                $restResponse
-            } elseif ($restResponse -and -not $restResponse.code) {
-                $outSecret = [PSCustomObject]@{
-                    SecretId                      = $restResponse.id
-                    SecretName                    = $restResponse.name
-                    TemplateId                    = $restResponse.secretTemplateId
-                    FolderId                      = if ($restResponse.folderId -eq -1) { $null } else { $restResponse.folderId }
-                    Status                        = if ($restResponse.active) { "Active" } else { "Inactive" }
-                    LauncherConnectSecretId       = if ($restResponse.launcherConnectAsSecretId -eq -1) { $null } else { $restResponse.launcherConnectAsSecretId }
-                    Restricted                    = $restResponse.isRestricted
-                    OutOfSync                     = $restResponse.isOutOfSync
-                    OutOfSyncReason               = $restResponse.outOfSyncReason
-                    AutoChangeEnabled             = $restResponse.autoChangeEnabled
-                    AutoChangeNextPassword        = $restResponse.AutoChangeNextPassword
-                    ApprovalForAccessRequired     = $restResponse.requiresApprovalForAccess
-                    CommentRequired               = $restResponse.requiresComment
-                    CheckedOut                    = $restResponse.checkedOut
-                    CheckoutEnabled               = $restResponse.checkOutEnabled
-                    CheckoutUserId                = if ($restResponse.checkOutUserId -eq -1) { $null } else { $restResponse.checkOutUserId }
-                    CheckoutUserName              = if ($restResponse.checkOutUserDisplayName -eq -1) { $null } else { $restResponse.checkOutUserDisplayName }
-                    CheckoutIntervalMinutes       = if ($restResponse.CheckoutIntervalMinutes -eq -1) { $null } else { $restResponse.checkOutIntervalMinutes }
-                    CheckoutChangePassword        = $restResponse.checkOutChangePasswordEnabled
-                    AccessRequestWorkflowMapId    = if ($restResponse.accessRequestWorkflowMapId -eq -1) { $null } else { $restResponse.accessRequestWorkflowMapId }
-                    Proxy                         = $restResponse.proxyEnabled
-                    SessionRecording              = $restResponse.sessionRecordingEnabled
-                    SSHCommandsRestricted         = $restResponse.restrictSshCommands
-                    SSHCommandsOwnersUnrestricted = $restResponse.allowOwnersUnrestrictedSshCommands
-                    DoubleLockEnabled             = $restResponse.isDoubleLock
-                    DoubleLockId                  = if ($restResponse.doubleLockId -eq -1) { $null } else { $restResponse.doubleLockId }
-                    InheritsPermissions            = $restResponse.enableInheritPermissions
-                    InheritsSecretPolicy           = if ($restResponse.enableInheritSecretPolicy -eq -1) { $null } else { $restResponse.enableInheritSecretPolicy }
-                    SiteId                        = $restResponse.siteId
-                    SecretPolicyId                = if ($restResponse.secretPolicyId -eq -1) { $null } else { $restResponse.secretPolicyId }
-                    HeartbeatStatus               = $restResponse.lastHeartBeatStatus
-                    HeartbeatDate                 = [datetime]$restResponse.lastHeartBeatCheck
-                    PasswordChangeFailedCount     = $restResponse.failedPasswordChangeAttempts
-                    PasswordChangeAttempt         = [datetime]$restResponse.lastPasswordChangeAttempt
-                    TemplateName                  = $restResponse.secretTemplateName
-                    PasswordTypeWebscriptId       = if ($restResponse.passwordTypeWebScriptId -eq -1) { $null } else { $restResponse.passwordTypeWebScriptId }
+        if ($tssParams.Contains('TssSession') -and $TssSession.IsValidSession()) {
+            foreach ($secret in $Id) {
+                $restResponse = $null
+                $errorResponse = $null
+                $uri = $TssSession.SecretServerUrl + ($TssSession.ApiVersion, "secrets", $secret.ToString() -join '/')
+                if ($Comment) {
+                    $uri = $uri, "restricted" -join "/"
+                    $body = "{'comment':'$Comment', 'includeInactive':'$true'}"
+                    $invokeParams.Uri = $Uri
+                    $invokeParams.Method = 'POST'
+                    $invokeParams.Body = $body
+                } else {
+                    $uri = $uri, "includeInactive=true" -join "?"
+                    $invokeParams.Uri = $Uri
+                    $invokeParams.Method = 'GET'
                 }
-                foreach ($itemDetail in $restResponse.items) {
-                    $name = $itemDetail.fieldName
-                    $value = $itemDetail.itemValue
-                    $outSecret.PSObject.Properties.Add([PSNoteProperty]::new($name,$value))
-                }
-                $properties = $outSecret.PSObject.Properties | Sort-Object Name
-                $final = [PSCustomObject]@{ }
-                foreach ($prop in $properties) {
-                    $final.PSObject.Properties.Add([PSNoteProperty]::new($prop.Name,$prop.Value))
-                }
-                $final
-            }
 
-            if ($errorResponse) {
-                Write-Warning -Message "Issue retrieving secret [$secret]: $($errorResponse.message)"
+                $invokeParams.PersonalAccessToken = $TssSession.AccessToken
+                try {
+                    $restResponse = Invoke-TssRestApi @invokeParams -ErrorAction Stop
+                } catch {
+                    $errorResponse = $_.ErrorDetails.Message | ConvertFrom-Json
+                }
+
+                if ($Raw -and $restResponse) {
+                    $restResponse
+                } elseif ($restResponse -and -not $restResponse.code) {
+                    $outSecret = [PSCustomObject]@{
+                        PSTypeName                         = 'TssSecret'
+                        Id                                 = $restResponse.id
+                        Name                               = $restResponse.name
+                        SecretTemplateId                   = $restResponse.secretTemplateId
+                        SecretTemplateName                 = $restResponse.secretTemplateName
+                        FolderId                           = if ($restResponse.folderId -eq -1) { $null } else { $restResponse.folderId }
+                        Active                             = $restResponse.active
+                        LauncherConnectSecretId            = if ($restResponse.launcherConnectAsSecretId -eq -1) { $null } else { $restResponse.launcherConnectAsSecretId }
+                        IsRestricted                       = $restResponse.isRestricted
+                        IsOutOfSync                        = $restResponse.isOutOfSync
+                        OutOfSyncReason                    = $restResponse.outOfSyncReason
+                        AutoChangeEnabled                  = $restResponse.autoChangeEnabled
+                        AutoChangeNextPassword             = $restResponse.AutoChangeNextPassword
+                        RequiresApprovalForAccess          = $restResponse.requiresApprovalForAccess
+                        RequiresComment                    = $restResponse.requiresComment
+                        CheckedOut                         = $restResponse.checkedOut
+                        CheckoutEnabled                    = $restResponse.checkOutEnabled
+                        CheckoutUserId                     = if ($restResponse.checkOutUserId -eq -1) { $null } else { $restResponse.checkOutUserId }
+                        CheckoutUserDisplayName            = if ($restResponse.checkOutUserDisplayName -eq -1) { $null } else { $restResponse.checkOutUserDisplayName }
+                        CheckoutIntervalMinutes            = if ($restResponse.CheckoutIntervalMinutes -eq -1) { $null } else { $restResponse.checkOutIntervalMinutes }
+                        CheckoutChangePassword             = $restResponse.checkOutChangePasswordEnabled
+                        AccessRequestWorkflowMapId         = if ($restResponse.accessRequestWorkflowMapId -eq -1) { $null } else { $restResponse.accessRequestWorkflowMapId }
+                        ProxyEnabled                       = $restResponse.proxyEnabled
+                        SessionRecordingEnabled            = $restResponse.sessionRecordingEnabled
+                        RestrictSshCommands                = $restResponse.restrictSshCommands
+                        AllowOwnersUnrestrictedSshCommands = $restResponse.allowOwnersUnrestrictedSshCommands
+                        IsDoubleLock                       = $restResponse.isDoubleLock
+                        DoubleLockId                       = if ($restResponse.doubleLockId -eq -1) { $null } else { $restResponse.doubleLockId }
+                        EnableInheritsPermissions          = $restResponse.enableInheritPermissions
+                        EnableInheritsSecretPolicy         = if ($restResponse.enableInheritSecretPolicy -eq -1) { $null } else { $restResponse.enableInheritSecretPolicy }
+                        SiteId                             = $restResponse.siteId
+                        SecretPolicyId                     = if ($restResponse.secretPolicyId -eq -1) { $null } else { $restResponse.secretPolicyId }
+                        LastHeartbeatStatus                = $restResponse.lastHeartBeatStatus
+                        LastHeartbeatCheck                 = [datetime]$restResponse.lastHeartBeatCheck
+                        FailedPasswordChangeAttempts       = $restResponse.failedPasswordChangeAttempts
+                        LastPasswordChangeAttempt          = [datetime]$restResponse.lastPasswordChangeAttempt
+                        PasswordTypeWebscriptId            = if ($restResponse.passwordTypeWebScriptId -eq -1) { $null } else { $restResponse.passwordTypeWebScriptId }
+                    }
+
+                    $items = foreach ($itemDetail in $restResponse.items) {
+                        [pscustomobject]@{
+                            PSTypeName       = 'TssSecretItem'
+                            ItemId           = $itemDetail.itemId
+                            ItemValue        = $itemDetail.itemValue
+                            FieldId          = $itemDetail.fieldId
+                            FieldName        = $itemDetail.fieldName
+                            Slug             = $itemDetail.slug
+                            FieldDescription = $itemDetail.fieldDescription
+                            IsFile           = $itemDetail.isFile
+                            FileAttachmentId = $itemDetail.fileAttachmentId
+                            FileName         = $itemDetail.fileName
+                            IsNotes          = $itemDetail.isNotes
+                            IsPassword       = $itemDetail.isPassword
+                        }
+                    }
+                    $outSecret.PSObject.Properties.Add([PSNoteProperty]::new('Items',$items))
+                    $outSecret
+                }
+
+                if ($errorResponse) {
+                    Write-Warning -Message "Issue retrieving secret [$secret]: $($errorResponse.message)"
+                }
+                if ($restResponse.code) {
+                    Write-Warning -Message "Issue retrieving secret [$secret]: $($restResponse.message)"
+                }
             }
-            if ($restResponse.code) {
-                Write-Warning -Message "Issue retrieving secret [$secret]: $($restResponse.message)"
-            }
+        } else {
+            Write-Warning "No valid session found"
         }
     }
 }
