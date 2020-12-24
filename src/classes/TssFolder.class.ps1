5eb57ef254fdd3cc8f724e44668a3b44b98d9b6a
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2020-12-24 16:46:29 -0600

    fixing file encoding

---
+++ src/classes/TssFolder.class.ps1
@@ -0,0 +1,16 @@
+﻿class TssFolderTemplate {
+    [int]$Id
+    [string]$Name
+    [int]$SecretCount
+    [boolean]$Active
+}
+
+class TssFolder {
+    [int]$Id
+    [string]$FolderName
+    [string]$FolderPath
+    [int]$ParentFolderId
+    [int]$FolderTypeId
+    [int]$SecretPolicyId
+    [boolean]$InheritSecretPolicy
+}
