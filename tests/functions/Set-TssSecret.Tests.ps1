5bf0caa78ef94efb00354364a69305e62c63a174
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2020-12-26 11:35:38 -0600

    Set-TssSecret - tests add cleanup on session

--- tests/functions/Set-TssSecret.Tests.ps1
+++ tests/functions/Set-TssSecret.Tests.ps1
@@ -22,23 +22,24 @@ Describe "$commandName verify parameters" {
 
 Describe "$commandName works" {
     BeforeDiscovery {
-        $session = New-TssSession -SecretServer $ssVault1 -Credential $vault1Cred
+        $session = New-TssSession -SecretServer $ss -Credential $ssCred
 
         $invokeParams = @{
-            Uri = "$ssVault1/api/v1/folders?take=$($session.take)"
+            Uri = "$ss/api/v1/folders?take=$($session.take)"
             Method = 'GET'
             ExpandProperty = 'records'
             PersonalAccessToken = $session.AccessToken
         }
         $getFolders = Invoke-TssRestApi @invokeParams
         $tssFolder = $getFolders.Where({$_.folderPath -match 'tss_module_testing\\SetTssSecret'})
+        $session.SessionExpire()
     }
     Context "Set a secret field and property" {
         BeforeDiscovery {
-            $session = New-TssSession -SecretServer $ssVault1 -Credential $vault1Cred
+            $session = New-TssSession -SecretServer $ss -Credential $ssCred
 
             $invokeParams = @{
-                Uri = "$ssVault1/api/v1/secrets/lookup?take=$($session.take)&folderid=$($TssFolder.id)"
+                Uri = "$ss/api/v1/secrets/lookup?take=$($session.take)&folderid=$($TssFolder.id)"
                 Method = 'GET'
                 PersonalAccessToken = $session.AccessToken
                 ExpandProperty = 'records'
@@ -47,32 +48,43 @@ Describe "$commandName works" {
 
             $secretId = $getSecrets.Where({$_.value -match 'Test Setting Field and Property'}).id
             $invokeParams = @{
-                Uri = "$ssVault1/api/v1/secrets/$secretId"
+                Uri = "$ss/api/v1/secrets/$secretId"
                 Method = 'GET'
                 PersonalAccessToken = $session.AccessToken
             }
             $setSecret = Invoke-TssRestApi @invokeParams
+            $session.SessionExpire()
         }
         It "Should set the value on the Notes field" -TestCases $setSecret {
-            $session = New-TssSession -SecretServer $ssVault1 -Credential $vault1Cred
+            $session = New-TssSession -SecretServer $ss -Credential $ssCred
             $valueField = "your friendly local PowerShell Module"
             Set-TssSecret -TssSession $session -Id $_.id -Field Notes -Value $valueField | Should -BeNullOrEmpty
+
+            $session.SessionExpire()
         }
         It "Should Clear the Notes field" -TestCases $setSecret {
-            $session = New-TssSession -SecretServer $ssVault1 -Credential $vault1Cred
+            $session = New-TssSession -SecretServer $ss -Credential $ssCred
             Set-TssSecret -TssSession $session -Id $_.Id -Field Notes -Clear | Should -BeNullOrEmpty
+
+            $session.SessionExpire()
         }
         It "Should set EmailWhenChanged" -TestCases $setSecret {
-            $session = New-TssSession -SecretServer $ssVault1 -Credential $vault1Cred
+            $session = New-TssSession -SecretServer $ss -Credential $ssCred
             Set-TssSecret -TssSession $session -Id $_.Id -EmailWhenChanged:$false | Should -BeNullOrEmpty
+
+            $session.SessionExpire()
         }
         It "Should set EmailWhenViewed" -TestCases $setSecret {
-            $session = New-TssSession -SecretServer $ssVault1 -Credential $vault1Cred
+            $session = New-TssSession -SecretServer $ss -Credential $ssCred
             Set-TssSecret -TssSession $session -Id $_.Id -EmailWhenViewed:$false | Should -BeNullOrEmpty
+
+            $session.SessionExpire()
         }
         It "Should set EmailWhenHeartbeatFails" -TestCases $setSecret {
-            $session = New-TssSession -SecretServer $ssVault1 -Credential $vault1Cred
+            $session = New-TssSession -SecretServer $ss -Credential $ssCred
             Set-TssSecret -TssSession $session -Id $_.Id -EmailWhenHeartbeatFails:$false | Should -BeNullOrEmpty
+
+            $session.SessionExpire()
         }
     }
 }
--- tests/functions/constants.ps1
+++ tests/functions/constants.ps1
@@ -1,9 +1,5 @@
-$script:ssVault1 = 'http://vault1'
-$script:ssVault2 = 'http://vault2'
-$script:ssVault3 = 'http://vault3'
+$script:ss = 'http://vault1'
 $script:ssc = 'https://proservices.secretservercloud.com'
 
-$script:vault1Cred = $secretVault1Cred
-$script:vault2Cred = $secretVault2Cred
-$script:vault3Cred = $secretVault3Cred
-$script:sscCred = $secretCloudCred
+$script:ssCred = $secretVault1Cred
+# $script:ssCred = $secretCloudCred
