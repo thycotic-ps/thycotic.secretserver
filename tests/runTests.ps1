0f2fa3577ccb63895ccc0ac9ced8f7b597c696ee
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-01-06 09:35:28 -0600

    Add script to run tests

---
+++ tests/runTests.ps1
@@ -0,0 +1,15 @@
+﻿$testRootPath = $PSScriptRoot
+$modulePsd1 = [IO.Path]::Combine(($PSScriptRoot.Trim("tests")), 'src\Thycotic.SecretServer.psd1')
+$module = Import-Module $modulePsd1 -Force -PassThru
+
+Write-Host "Testing module version $($module.Version)" -BackgroundColor DarkGreen -ForegroundColor Black
+try {
+    if (-not (Get-Module Pester)) {
+        Import-Module Pester -RequiredVersion 5.1.0
+    }
+} catch {
+    Write-Error "Pester 5.1.0 was not imported, please correct"
+    return
+}
+
+Invoke-Pester -Path $testRootPath -Output Minimal
