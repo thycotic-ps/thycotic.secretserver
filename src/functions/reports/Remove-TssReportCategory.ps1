7f11f4d5f4dc72f4c0fdc29a0dc3cb07125b7def
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-01-10 12:41:11 -0600

    Remove-TssReportCategory - fixes #40

---
+++ src/functions/reports/Remove-TssReportCategory.ps1
@@ -0,0 +1,70 @@
+﻿function Remove-TssReportCategory {
+    <#
+    .SYNOPSIS
+    Delete a report category
+
+    .DESCRIPTION
+    Deletes the report category, associated reports are changed to inactive and moved to the Delete category
+
+    .EXAMPLE
+    PS C:\> $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    PS C:\> Remove-TssReportCategory -TssSession $session -Id 11
+
+    A confirmation prompt will be shown.
+    Removes report category 11
+
+    .EXAMPLE
+    PS C:\> $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    PS C:\> Remove-TssReportCategory -TssSession $session -Id 21 -Confirm:$false
+
+    Confirmation prompt is bypassed
+    Removes report category 21
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    [OutputType('Boolean')]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,
+            ValueFromPipeline,
+            Position = 0)]
+        [TssSession]$TssSession,
+
+        # Category ID
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias('Id')]
+        [int[]]
+        $ReportCategoryId
+    )
+    begin {
+        $tssParams = . $GetParams $PSBoundParameters 'Remove-TssReportCategory'
+        $invokeParams = @{ }
+    }
+
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.Contains('TssSession') -and $TssSession.IsValidSession()) {
+            foreach ($id in $ReportCategoryId) {
+                $uri = $TssSession.ApiUrl, 'reports/categories', $id.ToString() -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'DELETE'
+
+                $invokeParams.PersonalAccessToken = $TssSession.AccessToken
+                Write-Verbose "$($invokeParams.Method) $uri"
+                if (-not $PSCmdlet.ShouldProcess($id, "$($invokeParams.Method) $uri")) { return }
+                try {
+                    Invoke-TssRestApi @invokeParams
+                    $true
+                } catch {
+                    Write-Warning "Issue removing [$id]"
+                    $err = $_.ErrorDetails.Message
+                    Write-Error $err
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
---
+++ tests/reports/Remove-TssReportCategory.Tests.ps1
@@ -0,0 +1,49 @@
+﻿BeforeDiscovery {
+    $commandName = Split-Path ($PSCommandPath.Replace('.Tests.ps1','')) -Leaf
+    . ([IO.Path]::Combine([string]$PSScriptRoot, '..', 'constants.ps1'))
+}
+Describe "$commandName verify parameters" {
+    BeforeDiscovery {
+        [object[]]$knownParameters = 'TssSession','ReportCategoryId'
+        [object[]]$currentParams = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')).Parameters.Keys
+        [object[]]$commandDetails = [System.Management.Automation.CommandInfo]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')
+        $unknownParameters = Compare-Object -ReferenceObject $knownParameters -DifferenceObject $currentParams -PassThru
+    }
+    Context "Verify parmaeters" -Foreach @{currentParams = $currentParams} {
+        It "$commandName should contain <_> parameter" -TestCases $knownParameters {
+            $_ -in $currentParams | Should -Be $true
+        }
+        It "$commandName should not contain parameter: <_>" -TestCases $unknownParameters {
+            $_ | Should -BeNullOrEmpty
+        }
+    }
+    Context "Command specific details" {
+        It "$commandName should set OutputType to Boolean" -TestCases $commandDetails {
+            $_.OutputType.Name | Should -Be 'Boolean'
+        }
+    }
+}
+Describe "$commandName works" {
+    BeforeDiscovery {
+        $session = New-TssSession -SecretServer $ss -Credential $ssCred
+        $body = @{
+            reportCategoryDescription = 'tss test category to be deleted'
+            reportCategoryName = 'tss module testing to remove'
+            sortOrder = $null
+        }
+        $invokeParams = @{
+            Uri = "$ss/api/v1/reports/categories"
+            Body = $body
+            Method = 'POST'
+            PersonalAccessToken = $session.AccessToken
+        }
+        $createdCategory = Invoke-TssRestApi @invokeParams
+    }
+    Context "Checking" -Foreach @{createdCategory = $createdCategory} {
+        It "Should delete the category" {
+            Remove-TssReportCategory -TssSession $session -ReportCategoryId $createdCategory.reportCategoryId -Confirm:$false
+            $cat = Get-TssReportCategory -TssSession $session -ReportCategoryId $createdCategory.reportCategoryId -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+            $cat | Should -Not -BeNullOrEmpty
+        }
+    }
+}
