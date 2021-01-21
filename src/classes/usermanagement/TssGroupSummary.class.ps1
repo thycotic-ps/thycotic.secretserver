ba6017583db4bf4e76ac9c45b0280909b0fb914f
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-01-20 20:25:54 -0600

    Search-TssUserGroup - new command

---
+++ src/classes/usermanagement/TssGroupSummary.class.ps1
@@ -0,0 +1,22 @@
+﻿class TssGroupSummary {
+    # Created date
+    [datetime]$Created
+    # Domain Directory Group GUID
+    [string]$DomainGuid
+    # Directory Domain ID
+    [int]$DomainId
+    # Directory Domain Name
+    [string]$DomainName
+    # Group is Active
+    [boolean]$Enabled
+    # Group ID
+    [int]$Id
+    # Number of members
+    [int]$MemberCount
+    # Group Name
+    [string]$Name
+    # Group is synchronized with Active Directory
+    [boolean]$Synchronized
+    # Active Directory Sync will pull members for domain groups
+    [boolean]$SynchronizeNow
+}
---
+++ src/functions/usermanagement/Search-UserGroup.ps1
@@ -0,0 +1,103 @@
+﻿function Search-UserGroup {
+    <#
+    .SYNOPSIS
+    Search for user management groups
+
+    .DESCRIPTION
+    Search for user management groups
+
+    .EXAMPLE
+    PS C:\> $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    PS C:\> Search-UserGroup -TssSession $session
+
+    Return list of all groups found in Secret Server that account has access to manage
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('TssGroupSummary')]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,
+            ValueFromPipeline,
+            Position = 0)]
+        [TssSession]$TssSession,
+
+        # Active Directory Domain Id
+        [int]
+        $DomainId,
+
+        # Include inactive groups in results
+        [switch]
+        $IncludeInactive,
+
+        # Text to search for group name
+        [string]
+        $SearchText,
+
+        # Sort by specific property, default Name
+        [string]
+        $SortBy = 'Name',
+
+        # Output the raw response from the API endpoint
+        [switch]
+        $Raw
+    )
+    begin {
+        $tssParams = . $SearchUserGroupParams $PSBoundParameters
+        $invokeParams = @{ }
+
+        $groupParams = . $SearchUserGroupParams $PSBoundParameters
+        $groupParams.Remove('TssSession')
+        $groupParams.Remove('Raw')
+    }
+
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.Contains('TssSession') -and $TssSession.IsValidSession()) {
+            $uri = $TssSession.ApiUrl, 'groups' -join '/'
+            $uri += "?sortBy[0].direction=asc&sortBy[0].name=$SortBy&take=$($TssSession.Take)"
+
+            $filters = @()
+            if ($groupParams.Contains('DomainId')) {
+                $filters += "filter.domainId=$DomainId"
+            }
+            if ($groupParams.Contains('IncludeInactive')) {
+                $filters += "filter.includeInactive=$IncludeInactive"
+            }
+            if ($groupParams.Contains('SearchText')) {
+                $filters += "filter.searchText=$SearchText"
+            }
+            if ($filters) {
+                $uriFilter = $filters -join '&'
+                Write-Verbose "Filters: $uriFilter"
+                $uri = $uri, $uriFilter -join '&'
+            }
+
+            $invokeParams.Uri = $uri
+            $invokeParams.PersonalAccessToken = $TssSession.AccessToken
+            $invokeParams.Method = 'GET'
+            Write-Verbose "$($invokeParams.Method) $uri"
+            try {
+                $restReponse = Invoke-TssRestApi @invokeParams
+            } catch {
+                Write-Warning "Issue on search request"
+                $err = $_.ErrorDetails.Message
+                Write-Error $err
+            }
+
+            if ($tssParams['Raw']) {
+                return $restReponse
+            }
+            if ($restReponse.records.Count -le 0 -and $restReponse.records.Length -eq 0) {
+                Write-Warning "No groups found"
+            }
+            if ($restReponse.records) {
+                . $TssGroupSummaryObject $restReponse.records
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
