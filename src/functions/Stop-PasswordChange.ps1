a47e3f1a3dbf62ec799336df6b0f84fac543a1b5
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-01-27 18:42:20 -0600

    Stop-TssPasswordChange - fixes #48

---
+++ src/functions/Stop-PasswordChange.ps1
@@ -0,0 +1,77 @@
+﻿function Stop-PasswordChange {
+    <#
+    .SYNOPSIS
+    Stop a current password change
+
+    .DESCRIPTION
+    Stop a current passwor change
+
+    .EXAMPLE
+    PS C:\> $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    PS C:\> Stop-TssPasswordChange -TssSession $session -Id 46
+
+    Stop a current password change operation on secret 46
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,
+            ValueFromPipeline,
+            Position = 0)]
+        [TssSession]$TssSession,
+
+        # Secret Id
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("SecretId")]
+        [int[]]
+        $Id
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = @{ }
+    }
+
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            foreach ($secret in $Id) {
+                $restResponse = $null
+                $uri = $TssSession.ApiUrl, 'secrets', $secret.ToString(), 'stop-password-change' -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'POST'
+
+                $invokeParams.PersonalAccessToken = $TssSession.AccessToken
+                if (-not $PSCmdlet.ShouldProcess("$($invokeParams.Method) $uri")) { return }
+                Write-Verbose "$($invokeParams.Method) $uri with $body"
+                try {
+                    $restResponse = Invoke-TssRestApi @invokeParams
+                } catch {
+                    if (($_.ErrorDetails.Message | ConvertFrom-Json).Message) {
+                        [PSCustomObject]@{
+                            SecretId = $secret
+                            Status = $false
+                            Notes = ($_.ErrorDetails.Message | ConvertFrom-Json).Message
+                        }
+                    } else {
+                        Write-Warning "Issue removing [$secret]"
+                        $err = $_.ErrorDetails.Message
+                        Write-Error $err
+                    }
+                }
+
+                if ($restResponse.success) {
+                    [PSCustomObject]@{
+                        SecretId = $secret
+                        Status = $true
+                        Notes = $null
+                    }
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
---
+++ tests/functions/Stop-TssPasswordChange.Tests.ps1
@@ -0,0 +1,51 @@
+﻿BeforeDiscovery {
+    $commandName = Split-Path ($PSCommandPath.Replace('.Tests.ps1','')) -Leaf
+    . ([IO.Path]::Combine([string]$PSScriptRoot, '..', 'constants.ps1'))
+}
+Describe "$commandName verify parameters" {
+    BeforeDiscovery {
+        [object[]]$knownParameters = 'TssSession','Id'
+        [object[]]$currentParams = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')).Parameters.Keys
+        [object[]]$commandDetails = [System.Management.Automation.CommandInfo]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')
+        $unknownParameters = Compare-Object -ReferenceObject $knownParameters -DifferenceObject $currentParams -PassThru
+    }
+    Context "Verify parmaeters" -Foreach @{currentParams = $currentParams} {
+        It "$commandName should contain <_> parameter" -TestCases $knownParameters {
+            $_ -in $currentParams | Should -Be $true
+        }
+        It "$commandName should not contain parameter: <_>" -TestCases $unknownParameters {
+            $_ | Should -BeNullOrEmpty
+        }
+    }
+}
+Describe "$commandName works" {
+    BeforeDiscovery {
+        $session = New-TssSession -SecretServer $ss -Credential $ssCred
+
+        $invokeParams = @{
+            Uri = "$ss/api/v1/folders?take=$($session.take)"
+            ExpandProperty = 'records'
+            PersonalAccessToken = $session.AccessToken
+        }
+        $getFolders = Invoke-TssRestApi @invokeParams
+        $tssSecretFolder = $getFolders.Where({$_.folderPath -match 'tss_module_testing\\GetTssSecret'})
+        $getSecrets = Invoke-TssRestApi -Uri "$ss/api/v1/secrets?take=$($session.take)&folderid=$($tssSecretFolder.id)" -Method Get -PersonalAccessToken $session.AccessToken -ExpandProperty records
+
+        $params = @{
+            TssSession = $session
+            Id = $getSecrets[0].id
+        }
+        $secret = Get-TssSecret @params | Stop-TssPasswordChange -TssSession $session
+        $session.SessionExpire()
+
+        $props = 'SecretId','Status','Notes'
+    }
+    Context "Checking" -Foreach @{secret = $secret} {
+        It "Should not be empty" {
+            $secret | Should -Not -BeNullOrEmpty
+        }
+        It "$commandName should get property <_> at minimum" -TestCases $props {
+            $secret.PSObject.Properties.Name | Should -Contain $_
+        }
+    }
+}
