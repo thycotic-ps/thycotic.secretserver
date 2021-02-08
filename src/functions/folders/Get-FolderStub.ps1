9f535aa341a046a5b73ffdeb5c64398fa27760e1
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-02-07 22:05:57 -0600

    Get-TssFolderStub - closes #63

---
+++ src/functions/folders/Get-FolderStub.ps1
@@ -0,0 +1,57 @@
+﻿function Get-FolderStub {
+    <#
+    .SYNOPSIS
+    Get template for new secret folder
+
+    .DESCRIPTION
+    Get template for new secret folder
+
+    .EXAMPLE
+    PS C:\> $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    PS C:\> Get-TssFolderStub -TssSession $session
+
+    Returns folder template as an object
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('TssFolder')]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,
+            ValueFromPipeline,
+            Position = 0)]
+        [TssSession]$TssSession
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = @{ }
+    }
+
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            $restResponse = $null
+            $uri = $TssSession.ApiUrl, 'folders/stub' -join '/'
+            $invokeParams.Uri = $uri
+            $invokeParams.Method = 'GET'
+
+            $invokeParams.PersonalAccessToken = $TssSession.AccessToken
+            Write-Verbose "$($invokeParams.Method) $uri with $body"
+            try {
+                $restResponse = Invoke-TssRestApi @invokeParams
+            } catch {
+                Write-Warning "Issue getting folder stub"
+                $err = $_.ErrorDetails.Message
+                Write-Error $err
+            }
+
+            if ($restResponse) {
+                . $TssFolderObject $restResponse
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
---
+++ tests/folders/Get-TssFolderStub.Tests.ps1
@@ -0,0 +1,44 @@
+﻿BeforeDiscovery {
+    $commandName = Split-Path ($PSCommandPath.Replace('.Tests.ps1','')) -Leaf
+    . ([IO.Path]::Combine([string]$PSScriptRoot, '..', 'constants.ps1'))
+}
+Describe "$commandName verify parameters" {
+    BeforeDiscovery {
+        [object[]]$knownParameters = 'TssSession'
+        [object[]]$currentParams = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')).Parameters.Keys
+        [object[]]$commandDetails = [System.Management.Automation.CommandInfo]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')
+        $unknownParameters = Compare-Object -ReferenceObject $knownParameters -DifferenceObject $currentParams -PassThru
+    }
+    Context "Verify parmaeters" -Foreach @{currentParams = $currentParams} {
+        It "$commandName should contain <_> parameter" -TestCases $knownParameters {
+            $_ -in $currentParams | Should -Be $true
+        }
+        It "$commandName should not contain parameter: <_>" -TestCases $unknownParameters {
+            $_ | Should -BeNullOrEmpty
+        }
+    }
+    Context "Command specific details" {
+        It "$commandName should set OutputType to TssFolder" -TestCases $commandDetails {
+            $_.OutputType.Name | Should -Be 'TssFolder'
+        }
+    }
+}
+Describe "$commandName works" {
+    BeforeDiscovery {
+        $session = New-TssSession -SecretServer $ss -Credential $ssCred
+        $folderStub = Get-TssFolderStub -TssSession $session
+        $session.SessionExpire()
+        $props = 'FolderId', 'ParentFolderId', 'FolderName'
+    }
+    Context "Checking" -Foreach @{folderStub = $folderStub} {
+        It "Should not be empty" {
+            $folderStub | Should -Not -BeNullOrEmpty
+        }
+        It "Should output <_> property" -TestCases $props {
+            $folderStub.PSObject.Properties.Name | Should -Contain $_
+        }
+        It "Should have ParentFolderId of -1 (negative one)" {
+            $folderStub.ParentFolderId | Should -Be -1
+        }
+    }
+}
