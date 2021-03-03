173bd417441e11fa362053589113cf8133483275
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-03-03 17:12:57 -0600

    Search-TssUser - closes #99

---
+++ src/classes/users/TssUserSummary.class.ps1
@@ -0,0 +1,37 @@
+class TssUserSummary {
+    [datetime]
+    $Created
+
+    [string]
+    $DisplayName
+
+    [int]
+    $DomainId
+
+    [string]
+    $DomainName
+
+    [string]
+    $EmailAddress
+
+    [boolean]
+    $Enabled
+
+    [int]
+    $Id
+
+    [boolean]
+    $IsApplicationAccount
+
+    [boolean]
+    $IsLockedOut
+
+    [datetime]
+    $LastLogin
+
+    [int]
+    $LoginFailures
+
+    [string]
+    $Username
+}
