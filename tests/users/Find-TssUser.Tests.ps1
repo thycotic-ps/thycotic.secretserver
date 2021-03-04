660c08248ef6115b489e6f21d1c397888de2fc61
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-03-04 17:11:17 -0600

    Find-TssUser - add params to test

--- tests/users/Find-TssUser.Tests.ps1
+++ tests/users/Find-TssUser.Tests.ps1
@@ -4,7 +4,7 @@ BeforeDiscovery {
 }
 Describe "$commandName verify parameters" {
     BeforeDiscovery {
-        [object[]]$knownParameters = 'TssSession'
+        [object[]]$knownParameters = 'TssSession', 'DomainId', 'IncludeInactive', 'Field', 'FindText', 'SortBy'
         [object[]]$currentParams = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')).Parameters.Keys
         [object[]]$commandDetails = [System.Management.Automation.CommandInfo]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')
         $unknownParameters = Compare-Object -ReferenceObject $knownParameters -DifferenceObject $currentParams -PassThru
