073d1b2d1d1cc6bfc9e27840d0c58d7ce3013d6b
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-03-17 22:28:15 -0500

    Enable-TssSecretEmail - closes #115

---
+++ src/functions/secrets/Enable-SecretEmail.ps1
@@ -0,0 +1,125 @@
+function Enable-SecretEmail {
+    <#
+    .SYNOPSIS
+    Enables the email setting for a Secret
+
+    .DESCRIPTION
+    Enables the email setting for a Secret
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Enable-TssSecretEmail -TssSession $session -Id 28 -When Viewed
+
+    Enable Secret 28's Email When Viewed setting
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Enable-TssSecretEmail -TssSession $session -Id 42,43,45 -When Viewed
+
+    Enable Email When Viewed setting on Secret IDs 42, 43, and 45
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/Disable-TssSecretEmail
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [TssSession]
+        $TssSession,
+
+        # Secret Id
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("SecretId")]
+        [int[]]
+        $Id,
+
+        # Email when changed to true
+        [switch]
+        $WhenChanged,
+
+        # Email when viewed to true
+        [switch]
+        $WhenViewed,
+
+        # Email when HB fails to true
+        [switch]
+        $WhenHeartbeatFails
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($secret in $Id) {
+                $uri = $TssSession.ApiUrl, 'secrets', $secret, 'email' -join '/'
+                $invokeParams.Uri = $uri
+
+                # data object for Email Settings
+                $emailBody = @{
+                    data = @{ }
+                }
+
+                if ($tssParams.ContainsKey('WhenChanged')) {
+                    $sendEmailWhenChanged = @{
+                        dirty = $true
+                        value = $true
+                    }
+                    $emailBody.data.Add('sendEmailWhenChanged',$sendEmailWhenChanged)
+                }
+                if ($tssParams.ContainsKey('WhenViewed')) {
+                    $sendEmailWhenViewed = @{
+                        dirty = $true
+                        value = $true
+                    }
+                    $emailBody.data.Add('sendEmailWhenViewed',$sendEmailWhenViewed)
+                }
+                if ($tssParams.ContainsKey('WhenHeartbeatFails')) {
+                    $sendEmailWhenHeartbeatFails = @{
+                        dirty = $true
+                        value = $true
+                    }
+                    $emailBody.data.Add('sendEmailWhenHeartbeatFails',$sendEmailWhenHeartbeatFails)
+                }
+
+
+                $invokeParams.Method = 'PATCH'
+                $invokeParams.Body = $emailBody | ConvertTo-Json -Depth 5
+
+                if ($restrictedParams.Count -gt 0) {
+                    if ($PSCmdlet.ShouldProcess("SecretId: $secret", "Pre-check out secret for setting email settings")) {
+                        . $CheckOutSecret $TssSession $tssParams $secret
+                    }
+                }
+                if ($PSCmdlet.ShouldProcess("SecretId: $secret", "$($invokeParams.Method) $uri with:`n$($invokeParams.Body)`n")) {
+                    Write-Verbose "$($invokeParams.Method) $uri with:`n$($invokeParams.Body)`n"
+                    try {
+                        $restResponse = Invoke-TssRestApi @invokeParams
+                    } catch {
+                        Write-Warning "Issue configuring [$secret] email settings, verify Email Server is configured in Secret Server"
+                        $err = $_
+                        . $ErrorHandling $err
+                    }
+
+                    if ($restResponse.PSObject.Properties.Name -contains 'sendEmailWhenChanged' -and $tssParams.ContainsKey('WhenChanged')) {
+                        Write-Verbose "Secret [$secret] email setting [Send Email When Changed] disabled"
+                    }
+                    if ($restResponse.PSObject.Properties.Name -contains 'sendEmailWhenViewed' -and $tssParams.ContainsKey('WhenViewed')) {
+                        Write-Verbose "Secret [$secret] email setting [Send Email When Viewed] disabled"
+                    }
+                    if ($restResponse.PSObject.Properties.Name -contains 'sendEmailWhenHeartbeatFails' -and $tssParams.ContainsKey('WhenHeartbeatFails')) {
+                        Write-Verbose "Secret [$secret] email setting [Send Email When Heartbeat Fails] disabled"
+                    }
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
---
+++ tests/secrets/Disable-TssSecretEmail.Tests.ps1
@@ -0,0 +1,62 @@
+BeforeDiscovery {
+    $commandName = Split-Path ($PSCommandPath.Replace('.Tests.ps1','')) -Leaf
+    . ([IO.Path]::Combine([string]$PSScriptRoot, '..', 'constants.ps1'))
+}
+Describe "$commandName verify parameters" {
+    BeforeDiscovery {
+        [object[]]$knownParameters = 'TssSession', 'Id', 'WhenHeartbeatFails', 'WhenViewed', 'WhenChanged'
+        [object[]]$currentParams = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')).Parameters.Keys
+        [object[]]$commandDetails = [System.Management.Automation.CommandInfo]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')
+        $unknownParameters = Compare-Object -ReferenceObject $knownParameters -DifferenceObject $currentParams -PassThru
+    }
+    Context "Verify parameters" -ForEach @{currentParams = $currentParams } {
+        It "$commandName should contain <_> parameter" -TestCases $knownParameters {
+            $_ -in $currentParams | Should -Be $true
+        }
+        It "$commandName should not contain parameter: <_>" -TestCases $unknownParameters {
+            $_ | Should -BeNullOrEmpty
+        }
+    }
+}
+Describe "$commandName functions" {
+    Context "Checking" {
+        BeforeAll {
+            $session = [pscustomobject]@{
+                ApiVersion   = 'api/v1'
+                Take         = 2147483647
+                SecretServer = 'http://alpha/'
+                ApiUrl       = 'http://alpha/api/v1'
+                AccessToken  = 'AgJf5YLFWtzw2UcBrM1s1KB2BGZ5Ufc4qLZ'
+                RefreshToken = '9oacYFZZ0YqgBNg0L7VNIF6-Z9ITE51Qplj'
+                TokenType    = 'bearer'
+                ExpiresIn    = 1199
+            }
+            Mock -Verifiable -CommandName Get-TssVersion -MockWith {
+                return @{
+                    Version = '10.9.000033'
+                }
+            }
+
+            $secretId = Get-Random -Maximum 30
+            Mock -Verifiable -CommandName Invoke-TssRestApi -ParameterFilter { $Uri -eq "$($session.ApiUrl)/secrets/$secretId/email"; $Method -eq 'PATCH' } -MockWith {
+                return [pscustomobject]@{
+                    sendEmailWhenViewed         = @{fieldInputType = ""; sortOrder = ""; value = $false; label = 'Send Email When Viewed'; name = ""; description = ""; readOnly = $false; placeholder = ""; hidden = $false; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    sendEmailWhenChanged        = @{fieldInputType = ""; sortOrder = ""; value = $false; label = 'Send Email When Changed'; name = ""; description = ""; readOnly = $false; placeholder = ""; hidden = $false; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    sendEmailWhenHeartbeatFails = @{fieldInputType = ""; sortOrder = ""; value = $false; label = 'Send Email When Heartbeat Fails'; name = ""; description = ""; readOnly = $false; placeholder = ""; hidden = $false; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    rdpLauncherSettings         = @{fieldInputType = ""; sortOrder = ""; value = ""; label = ""; name = ""; description = ""; readOnly = ""; placeholder = ""; hidden = $false; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    sshLauncherSettings         = @{fieldInputType = ""; sortOrder = ""; value = ""; label = ""; name = ""; description = ""; readOnly = $true; placeholder = ""; hidden = $true; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    oneTimePasswordSettings     = @{fieldInputType = ""; sortOrder = ""; value = ""; label = ""; name = ""; description = ""; readOnly = ""; placeholder = ""; hidden = $false; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    expirationType              = @{fieldInputType = ""; sortOrder = ""; value = 'Template'; label = 'Expiration Type'; name = ""; description = ""; readOnly = $false; placeholder = ""; hidden = ""; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    expirationDayInterval       = @{fieldInputType = ""; sortOrder = ""; value = ""; label = 'Expiration Day Interval'; name = ""; description = ""; readOnly = $false; placeholder = ""; hidden = ""; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    expirationDate              = @{fieldInputType = ""; sortOrder = ""; value = ""; label = 'Expiration Date'; name = ""; description = ""; readOnly = $false; placeholder = ""; hidden = ""; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                    expirationTemplateText      = @{fieldInputType = ""; sortOrder = ""; value = 'Expires every 30 day(s)'; label = 'Expiration Template Text'; name = ""; description = ""; readOnly = $false; placeholder = ""; hidden = ""; hideOnView = ""; hasHistory = ""; isRequired = ""; readOnlyReason = ""; helpLink = "" }
+                }
+            }
+            $object = Disable-TssSecretEmail -TssSession $session -Id $secretId -WhenChanged
+            Assert-VerifiableMock
+        }
+        It "Should not be empty" {
+            $object | Should -BeNullOrEmpty
+        }
+    }
+}
