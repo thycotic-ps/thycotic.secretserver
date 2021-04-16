5cf2157f3335e05d507d5b5eb075ea85c5f90c15
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-04-16 08:33:28 -0500

    Unlock-User - closes #159

---
+++ src/functions/users/Unlock-User.ps1
@@ -0,0 +1,78 @@
+function Unlock-User {
+    <#
+    .SYNOPSIS
+    Unlock a Secret Server User Account
+
+    .DESCRIPTION
+    Unlock a Secret Server User Account
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Unlock-TssUser -TssSession $session -Id 28
+
+    Unlock User 28
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/Unlock-TssUser
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/users/Unlock-User.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [TssSession]
+        $TssSession,
+
+        # User Id
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("UserId")]
+        [int[]]
+        $Id
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($user in $Id) {
+                $uri = $TssSession.ApiUrl, 'users', $user -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'PATCH'
+
+                $userBody = @{
+                    isLockedOut = @{
+                        dirty = $true
+                        value = $false
+                    }
+                }
+                $invokeParams.Body = $userBody | ConvertTo-Json
+                if ($PSCmdlet.ShouldProcess("SecretId: $user", "$($invokeParams.Method) $uri with:`n$($invokeParams.Body)`n")) {
+                    Write-Verbose "$($invokeParams.Method) $uri with:`n$($invokeParams.Body)`n"
+                    try {
+                        $restResponse = . $InvokeApi @invokeParams
+                    } catch {
+                        Write-Warning "Issue unlocking User [$user]"
+                        $err = $_
+                        . $ErrorHandling $err
+                    }
+
+                    if ($restResponse.isLockedOut -eq $false) {
+                        Write-Verbose "User [$user] unlocked"
+                    } else {
+                        Write-Warning "User [$user] has not been unlocked"
+                    }
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
---
+++ tests/users/UnLock-User.Tests.ps1
@@ -0,0 +1,58 @@
+BeforeDiscovery {
+    $commandName = Split-Path ($PSCommandPath.Replace('.Tests.ps1','')) -Leaf
+    . ([IO.Path]::Combine([string]$PSScriptRoot, '..', 'constants.ps1'))
+}
+Describe "$commandName verify parameters" {
+    BeforeDiscovery {
+        [object[]]$knownParameters = 'TssSession', 'Id'
+        [object[]]$currentParams = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')).Parameters.Keys
+        [object[]]$commandDetails = [System.Management.Automation.CommandInfo]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')
+        $unknownParameters = Compare-Object -ReferenceObject $knownParameters -DifferenceObject $currentParams -PassThru
+    }
+    Context "Verify parameters" -Foreach @{currentParams = $currentParams } {
+        It "$commandName should contain <_> parameter" -TestCases $knownParameters {
+            $_ -in $currentParams | Should -Be $true
+        }
+        It "$commandName should not contain parameter: <_>" -TestCases $unknownParameters {
+            $_ | Should -BeNullOrEmpty
+        }
+    }
+}
+Describe "$commandName functions" {
+    Context "Checking" {
+        BeforeAll {
+            $session = [pscustomobject]@{
+                ApiVersion   = 'api/v1'
+                Take         = 2147483647
+                SecretServer = 'http://alpha/'
+                ApiUrl       = 'http://alpha/api/v1'
+                AccessToken  = 'AgJf5YLChrisPine312UcBrM1s1KB2BGZ5Ufc4qLZ'
+                RefreshToken = '9oacYeah0YqgBNg0L7VinDiesel6-Z9ITE51Humus'
+                TokenType    = 'bearer'
+                ExpiresIn    = 1199
+            }
+            Mock -Verifiable -CommandName Invoke-RestMethod -ParameterFilter { $Uri -match '/version' } -MockWith {
+                return @{
+                   model = [pscustomobject]@{
+                       Version = '10.9.000033'
+                   }
+                }
+            }
+
+            $userId = 42
+            Mock -Verifiable -CommandName Invoke-RestMethod -ParameterFilter { $Uri -match "/users/$userId" } -MockWith {
+                return [pscustomobject]@{
+                    isLockedOut = $false
+                }
+            }
+            $object = Unlock-User -TssSession $session -Id $userId
+            Assert-VerifiableMock
+        }
+        It "Should be empty" {
+            $object | Should -BeNullOrEmpty
+        }
+        It "Should have called Invoke-RestMethod 2 times" {
+            Assert-MockCalled -CommandName Invoke-RestMethod -Times 2 -Scope Describe
+        }
+    }
+}
