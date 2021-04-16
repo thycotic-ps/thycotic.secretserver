b76de38d756a5e97276727dd52714250ae66bf0a
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-04-16 08:33:10 -0500

    Lock-User - closes #158

---
+++ src/functions/users/Lock-User.ps1
@@ -0,0 +1,78 @@
+function Lock-User {
+    <#
+    .SYNOPSIS
+    Lock a Secret Server User Account
+
+    .DESCRIPTION
+    Lock a Secret Server User Account
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Lock-TssUser -TssSession $session -Id 28
+
+    Lock User 28
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/Lock-TssUser
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/users/Lock-User.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [TssSession]
+        $TssSession,
+
+        # User Id
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("UserId")]
+        [int[]]
+        $Id
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($user in $Id) {
+                $uri = $TssSession.ApiUrl, 'users', $user -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'PATCH'
+
+                $userBody = @{
+                    isLockedOut = @{
+                        dirty = $true
+                        value = $true
+                    }
+                }
+                $invokeParams.Body = $userBody | ConvertTo-Json
+                if ($PSCmdlet.ShouldProcess("SecretId: $user", "$($invokeParams.Method) $uri with:`n$($invokeParams.Body)`n")) {
+                    Write-Verbose "$($invokeParams.Method) $uri with:`n$($invokeParams.Body)`n"
+                    try {
+                        $restResponse = . $InvokeApi @invokeParams
+                    } catch {
+                        Write-Warning "Issue locking User [$user]"
+                        $err = $_
+                        . $ErrorHandling $err
+                    }
+
+                    if ($restResponse.isLockedOut) {
+                        Write-Verbose "User [$user] locked out"
+                    } else {
+                        Write-Warning "User [$user] has not been locked out"
+                    }
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
