38e0b84774ad5f5d0bd1570502bef6c51ce5be4e
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-04-28 11:12:12 -0500

    Secret Template - Added New-SecretTemplateField and Add-SecretTemplateField

---
+++ src/functions/secret-templates/Add-SecretTemplateField.ps1
@@ -0,0 +1,82 @@
+function Add-SecretTemplateField {
+    <#
+    .SYNOPSIS
+    Add a new field to a Secret Template
+
+    .DESCRIPTION
+    Add a new field to a Secret Template
+
+    .EXAMPLE
+    $session = New-TssSession 'https://alpha/SecretServer' $ssCred
+    (Get-TssSecretTemplate -TssSession $session -Id 6042).Fields
+    $newField = New-TssSecretTemplateField -FieldName 'Additional Field' -Searchable
+    Add-TssSecretTemplateField -TssSession $session -Id 6042 -Field $newField
+    (Get-TssSecretTemplate -TssSession $session -Id 6042).Fields
+
+    Output the current fields for Secret Template 6042, create a new field named "Additional Field" that is searchable and add to the Secret Template 6042
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/Add-TssSecretTemplateField
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/secret-templates/Add-SecretTemplateField
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    [OutputType('TssSecretTemplate')]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [TssSession]
+        $TssSession,
+
+        # Template Stub object
+        [Parameter(Mandatory, Position = 1)]
+        [Alias('TemplateId')]
+        [int[]]
+        $Id,
+
+        # Fields, use New-TssSecretTemplateField to build this object
+        [Parameter(Mandatory, Position = 2)]
+        [TssSecretTemplateField]
+        $Field
+    )
+    begin {
+        $tssNewParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssNewParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($template in $Id) {
+                $restResponse = $null
+                $uri = $TssSession.ApiUrl, 'secret-templates', $template -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'POST'
+
+                $invokeParams.Body = ($Field | ConvertTo-Json)
+
+                Write-Verbose "Performing the operation $($invokeParams.Method) $uri with:`n $($invokeParams.Body)"
+                if (-not $PSCmdlet.ShouldProcess("Secret Template ID: $template", "$($invokeParams.Method) $uri with $($invokeParams.Body)")) { return }
+                try {
+                    $restResponse = . $InvokeApi @invokeParams
+                } catch {
+                    Write-Warning "Issue adding field to template [$template]"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+
+                if ($restResponse -and ($restResponse.name -eq $field.Name)) {
+                    Write-Verbose "Successfully added field to Secret Template [$template]"
+                } else {
+                    Write-Warning "Unable to add field to Secret Template [$template]"
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
--- src/functions/secret-templates/New-SecretTemplate.ps1
+++ src/functions/secret-templates/New-SecretTemplate.ps1
@@ -6,11 +6,29 @@ function New-SecretTemplate {
     .DESCRIPTION
     Create a new Secret Template
 
+    .EXAMPLE
+    $session = New-TssSession 'https://alpha/SecretServer' $ssCred
+    $copyTemplate = Get-TssSecretTemplate -TssSession $session -Id 6042
+    $copyTemplate.Name = 'Test Template - copy of 6042'
+    New-TssSecretTemplate -TssSession $session -Template $copyTemplate
+
+    Gets the Secret Template 6042, changing the name of the Template and then creating it
+
+    .EXAMPLE
+    $session = New-TssSession 'https://alpha/SecretServer' $ssCred
+    $fields = @()
+    $fields += New-TssSecretTemplateField -FieldName 'Field 1 Username' -Searchable
+    $fields += New-TssSecretTemplateField -FieldName 'Field 2 Password' -Type Password
+    $fields += New-TssSecretTemplateField -FieldName 'Field 3 URL' -Type Url -Searchable
+    New-TssSecretTemplate -TssSession $session -TemplateName 'Test Template 42' -TemplateField $fields
+
+    Creates a new template named "Test Template 42" with 3 fields
+
     .LINK
     https://thycotic-ps.github.io/thycotic.secretserver/commands/New-TssSecretTemplate
 
     .LINK
-    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/<folder>/New-SecretTemplate.ps1
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/secret-templates/New-SecretTemplate.ps1
 
     .NOTES
     Requires TssSession object returned by New-TssSession
@@ -19,19 +37,46 @@ function New-SecretTemplate {
     [OutputType('TssSecretTemplate')]
     param (
         # TssSession object created by New-TssSession for auth
-        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
         [TssSession]
         $TssSession,
 
         # Template Stub object
-        [Parameter(Mandatory,ValueFromPipeline,Position = 1)]
+        [Parameter(Mandatory, ValueFromPipeline, ParameterSetName = 'newcopy')]
         [Alias('TemplateStub')]
         [TssSecretTemplate]
-        $Template
+        $Template,
+
+        # Template Name
+        [Parameter(Mandatory, ParameterSetName = 'new')]
+        [string]
+        $TemplateName,
+
+        # Fields, use New-TssSecretTemplateField to build this object
+        [Parameter(Mandatory, ParameterSetName = 'new')]
+        [Alias('Fields')]
+        [TssSecretTemplateField[]]
+        $TemplateField
     )
     begin {
         $tssNewParams = $PSBoundParameters
         $invokeParams = . $GetInvokeTssParams $TssSession
+
+        $newcopyParamSet = . $ParameterSetParams $PSCmdlet.MyInvocation.MyCommand.Name 'newcopy'
+        $templateParams = @()
+        foreach ($t in $newcopyParamSet) {
+            if ($tssNewParams.ContainsKey($t)) {
+                $templateParams += $t
+            }
+        }
+
+        $newParamSet = . $ParameterSetParams $PSCmdlet.MyInvocation.MyCommand.Name 'new'
+        $fieldParams = @()
+        foreach ($f in $newParamSet) {
+            if ($tssNewParams.ContainsKey($f)) {
+                $fieldParams += $f
+            }
+        }
     }
     process {
         Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
@@ -42,14 +87,25 @@ function New-SecretTemplate {
             $invokeParams.Uri = $uri
             $invokeParams.Method = 'POST'
 
-            $invokeParams.Body = ($Template | ConvertTo-Json -Depth 6)
-            $templateName = $Template.Name
+            if ($templateParams.Count -gt 0) {
+                $TemplateName = $Template.Name
+                $invokeParams.Body = ($Template | ConvertTo-Json -Depth 6)
+            }
+            if ($fieldParams.Count -gt 0) {
+                $Template = [TssSecretTemplate]@{
+                    Id = 0
+                    Name = $TemplateName
+                    PasswordTypeId = 0
+                    Fields = $TemplateField
+                }
+                $invokeParams.Body = ($Template | ConvertTo-Json -Depth 6)
+            }
             Write-Verbose "Performing the operation $($invokeParams.Method) $uri with:`n $($invokeParams.Body)"
-            if (-not $PSCmdlet.ShouldProcess("Secret Template: $templateName", "$($invokeParams.Method) $uri with $($invokeParams.Body)")) { return }
+            if (-not $PSCmdlet.ShouldProcess("Secret Template: $TemplateName", "$($invokeParams.Method) $uri with $($invokeParams.Body)")) { return }
             try {
                 $restResponse = . $InvokeApi @invokeParams
             } catch {
-                Write-Warning "Issue creating template [$templateName]"
+                Write-Warning "Issue creating template [$TemplateName]"
                 $err = $_
                 . $ErrorHandling $err
             }
