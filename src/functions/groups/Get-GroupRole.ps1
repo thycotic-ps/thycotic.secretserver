f4e309958ab54daf706d4032a6b4c531cbb5e2f7
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-05-28 18:02:54 -0500

    Get-GroupRole - closes #54

---
+++ src/functions/groups/Get-GroupRole.ps1
@@ -0,0 +1,69 @@
+function Get-GroupRole {
+    <#
+    .SYNOPSIS
+    Get roles assigned to a Group
+
+    .DESCRIPTION
+    Get roles assigned to a Group
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Get-TssGroupRole -TssSession $session -Id 8
+
+    Return list of roles assigned to Group ID 8
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/Get-TssGroupRole
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/<folder>/Get-GroupRole.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('TssRoleSummary ')]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [TssSession]
+        $TssSession,
+
+        # Short description for parameter
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("GroupRoleId")]
+        [int[]]
+        $Id
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($group in $Id) {
+                $restResponse = $null
+                $uri = $TssSession.ApiUrl, 'groups', $group, 'roles' -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'GET'
+
+                Write-Verbose "Performing the operation $($invokeParams.Method) $uri"
+                try {
+                    $restResponse = . $InvokeApi @invokeParams
+                } catch {
+                    Write-Warning "Issue getting roles for group [$group]"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+
+                if ($restResponse.records) {
+                    [TssRoleSummary[]]$restResponse.records
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
