fb3a7eded3da0f46bdc74756afff0f75e30e5e89
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-06-01 11:36:43 -0500

    Search-SecretHook - closes #191

---
+++ src/classes/secret-hooks/TssSecretHookSummary.class.ps1
@@ -0,0 +1,34 @@
+class TssSecretHookSummary {
+    [int]
+    $SecretHookId
+
+    [int]
+    $HookId
+
+    [string]
+    $Name
+
+    [string]
+    $Description
+
+    [int]
+    $SortOrder
+
+    [string]
+    $PrePostOption
+
+    [string]
+    $EventActionName
+
+    [string]
+    $ScriptTypeName
+
+    [string]
+    $ScriptName
+
+    [boolean]
+    $Status
+
+    [boolean]
+    $StopOnFailure
+}
---
+++ src/functions/secret-hooks/Search-SecretHook.ps1
@@ -0,0 +1,69 @@
+function Search-SecretHook {
+    <#
+    .SYNOPSIS
+    Search all hooks for a specific Secret ID
+
+    .DESCRIPTION
+    Search all hooks for a specific Secret ID
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Search-TssSecretHook -TssSession $session -Id 82
+
+    Return all secret hooks associated with Secret ID 82
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/Search-TssSecretHook
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/secret-hooks/Search-SecretHook.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('TssSecretHookSummary')]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [TssSession]
+        $TssSession,
+
+        # Short description for parameter
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("SecretId")]
+        [int[]]
+        $Id
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($secret in $Id) {
+                $restResponse = $null
+                $uri = $TssSession.ApiUrl, 'secret-detail', $secret, 'hooks' -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'GET'
+
+                Write-Verbose "Performing the operation $($invokeParams.Method) $uri"
+                try {
+                    $restResponse = . $InvokeApi @invokeParams
+                } catch {
+                    Write-Warning "Issue getting hooks on Secret [$secret]"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+
+                if ($restResponse) {
+                    [TssSecretHookSummary[]]$restResponse
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
