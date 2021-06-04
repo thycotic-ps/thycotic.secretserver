26b343f5cec5882fda9f1c96eafe9ed8118b0e0c
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-06-03 22:42:01 -0500

    New-SecretDependency - closes #182

---
+++ src/functions/secret-dependencies/New-SecretDependency.ps1
@@ -0,0 +1,62 @@
+function New-SecretDependency {
+    <#
+    .SYNOPSIS
+    Create a new Secret Dependency
+
+    .DESCRIPTION
+    Create a new Secret Dependency
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/New-TssSecretDependency
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/secret-dependencies/New-SecretDependency.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    [OutputType('TssSecretDependency')]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [TssSession]
+        $TssSession,
+
+        # Secret Dependency Stub object
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [TssSecretDependency]
+        $DependencyStub
+    )
+    begin {
+        $tssNewParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssNewParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            $restResponse = $null
+            $uri = $TssSession.ApiUrl, 'secret-dependencies' -join '/'
+            $invokeParams.Uri = $uri
+            $invokeParams.Method = 'POST'
+            $invokeParams.Body = ($SecretDependencyStub | ConvertTo-Json)
+
+            Write-Verbose "Performing the operation $($invokeParams.Method) $uri with:`n $newBody"
+            if (-not $PSCmdlet.ShouldProcess("", "$($invokeParams.Method) $uri with $($invokeParams.Body)")) { return }
+            try {
+                $restResponse = . $InvokeApi @invokeParams
+            } catch {
+                Write-Warning "Issue creating dependency on Secret [$($DependencyStub.SecretId)]"
+                $err = $_
+                . $ErrorHandling $err
+            }
+
+            if ($restResponse) {
+                [TssSecretDependency]$restResponse
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
