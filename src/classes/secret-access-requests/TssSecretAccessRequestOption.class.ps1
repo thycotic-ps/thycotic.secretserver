de809d171d5a0515c151a6c6fbc17db48bdbdea2
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-06-10 09:17:28 -0500

    Get-SecretAccessRequestOption - new command

---
+++ src/classes/secret-access-requests/TssSecretAccessRequestOption.class.ps1
@@ -0,0 +1,31 @@
+class TssSecretAccessRequestOption {
+    [int]
+    $CustomCheckoutIntervalDays
+
+    [int]
+    $CustomCheckoutIntervalHours
+
+    [int]
+    $CustomCheckoutIntervalMinutes
+
+    [boolean]
+    $EditorsAlsoRequireApproval
+
+    [boolean]
+    $EnableDoubleLock
+
+    [boolean]
+    $EnableRequiresApprovalForAccess
+
+    [boolean]
+    $IsDefaultCheckoutInterval
+
+    [boolean]
+    $OwnersAndApproversAlsoRequireApproval
+
+    [boolean]
+    $RequireCheckout
+
+    [boolean]
+    $RequireCommentTicketNumber
+}
