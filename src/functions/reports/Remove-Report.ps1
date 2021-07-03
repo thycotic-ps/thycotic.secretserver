2cf097da80b3969151aec50eb77b3d3682fcbce3
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-07-02 21:27:32 -0500

    Remove-Report - new command fixes #42

---
+++ src/functions/reports/Remove-Report.ps1
@@ -0,0 +1,73 @@
+function Remove-Report {
+    <#
+    .SYNOPSIS
+    Remove a report.
+
+    .DESCRIPTION
+    Remove a report.
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Remove-TssReport -TssSession $session -Id 4
+
+    Remove Report ID 4.
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/Remove-TssReport
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/repors/Remove-Report.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    [OutputType('TssDelete')]
+    param (
+        # TssSession object created by New-TssSession for auth
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [TssSession]
+        $TssSession,
+
+        # Short description for parameter
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("ReportId")]
+        [int[]]
+        $Id
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($report in $Id) {
+                $restResponse = $null
+                $uri = $TssSession.ApiUrl, 'reports', $report -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'DELETE'
+
+                if (-not $PSCmdlet.ShouldProcess("Report ID: $report","$($invokeParams.Method) $uri")) { return }
+                Write-Verbose "Performing the operation $($invokeParams.Method) $uri with $body"
+                try {
+                    $restResponse = . $InvokeApi @invokeParams
+                } catch {
+                    Write-Warning "Issue removing report [$report]"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+
+                if ($restResponse) {
+                    [TssDelete]@{
+                        Id = $report
+                        ObjectType = 'Report'
+                    }
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
