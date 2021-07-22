3f0dc35bece1eb8735869d346fe0c49464e387cf
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-07-21 21:57:02 -0500

    Secret class - GetFieldValue method issue with empty value

--- src/Thycotic.SecretServer/classes/secrets/Secret.cs
+++ src/Thycotic.SecretServer/classes/secrets/Secret.cs
@@ -115,6 +115,17 @@ namespace Thycotic.PowerShell.Secrets
             }
         }
 
+        public void UpdateFieldId(string SlugName, int OrginalId, int NewId)
+        {
+            foreach (var item in this.Items)
+            {
+                if (item.Slug.Equals(SlugName) && item.FieldId.Equals(OrginalId))
+                {
+                    item.FieldId = NewId;
+                    break;
+                }
+            }
+        }
         public List<FileField> GetFileFields()
         {
             List<FileField> files = new List<FileField>();
