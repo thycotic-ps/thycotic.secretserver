e448c8c773acfb1206d1631f4179acedcd1b998c
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-07-30 09:25:51 -0500

    Get-TssEventPipelinePolicy - new command

---
+++ src/functions/event-pipeline-policy/Get-TssEventPipelinePolicy.ps1
@@ -0,0 +1,69 @@
+function Get-TssEventPipelinePolicy {
+    <#
+    .SYNOPSIS
+    Get an Event Pipeline Policy by ID
+
+    .DESCRIPTION
+    Get an Event Pipeline Policy by ID
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Get-TssEventPipelinePolicy -TssSession $session -Id 4
+
+    Get Event Pipeline Policy ID 4
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/event-pipeline-policy/Get-TssEventPipelinePolicy
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/event-pipeline-policy/Get-TssEventPipelinePolicy.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('Thycotic.PowerShell.EventPipelinePolicy.Policy')]
+    param (
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession,
+
+        # Short description for parameter
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("EventPipelinePolicyId")]
+        [int[]]
+        $Id
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($policy in $Id) {
+                $restResponse = $null
+                $uri = $TssSession.ApiUrl, 'event-pipeline-policy', $policy -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'GET'
+
+                Write-Verbose "Performing the operation $($invokeParams.Method) $uri"
+                try {
+                    $restResponse = . $InvokeApi @invokeParams
+                } catch {
+                    Write-Warning "Issue getting Event Pipeline Policy [$policy]"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+
+                if ($restResponse) {
+                    [Thycotic.PowerShell.EventPipelinePolicy.Policy]$restResponse
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
