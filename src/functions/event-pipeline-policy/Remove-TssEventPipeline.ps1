4712c0e7c784f62e455d7511481ae1b1d4e6925f
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-08-04 21:16:38 -0500

    Add/Remove-TssEventPipeline - new command to add/remove pipelines from an Event Pipeline Policy

---
+++ src/functions/event-pipeline-policy/Remove-TssEventPipeline.ps1
@@ -0,0 +1,75 @@
+function Remove-TssEventPipeline {
+    <#
+    .SYNOPSIS
+    Remove an Event Pipeline from a specific Event Pipeline Policy
+
+    .DESCRIPTION
+    Remove an Event Pipeline from a specific Event Pipeline Policy
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Remove-TssEventPipeline -TssSession $session -PipelineId 42 -PolicyId 3
+
+    Remove Event Pipeline 42 from the Event Pipeline Policy 3
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/event-pipeline-policy/Remove-TssEventPipeline
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/event-pipeline-policy/Remove-TssEventPipeline.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession,
+
+        # Event Pipeline Policy ID
+        [Parameter(Mandatory,ValueFromPipeline,Position = 1)]
+        [Alias('EventPipelinePolicyId')]
+        [int]
+        $PolicyId,
+
+        # Event Pipeline ID
+        [Parameter(Mandatory,ValueFromPipeline,Position = 2)]
+        [Alias("EventPipelineId")]
+        [int[]]
+        $PipelineId
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeTssParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000000' $PSCmdlet.MyInvocation
+            foreach ($pipeline in $PipelineId) {
+                $restResponse = $null
+                $uri = $TssSession.ApiUrl, 'event-pipeline-policy',$PolicyId, 'pipeline', $pipeline -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'DELETE'
+
+                if (-not $PSCmdlet.ShouldProcess($pipeline,"$($invokeParams.Method) $uri")) { return }
+                Write-Verbose "Performing the operation $($invokeParams.Method) $uri"
+                try {
+                    $restResponse = . $InvokeApi @invokeParams
+                } catch {
+                    Write-Warning "Issue removing Event Pipeline [$pipeline] from Event Pipeline Policy [$PolicyId]"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+
+                if ($restResponse) {
+                    Write-Verbose "Event Pipeline [$pipeline] successfully removed from Event Pipeline Policy [$PolicyId]"
+                }
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
