da45da6d5b3a65ab622520ffccecdda40d25ea0c
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-08-04 20:00:40 -0500

    Invoke-TssApi - internal cmdlet for web request Additional scripts added to accomodate processing web request output

---
+++ src/Thycotic.SecretServer/classes/common/RequestStatus.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace Thycotic.PowerShell.Common
+{
+    public class RequestStatus
+    {
+        public bool IsSuccessful { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusDescription { get; set; }
+        public string ResponseStatus { get; set; }
+    }
+}
---
+++ src/parts/ProcessResponse.ps1
@@ -0,0 +1,27 @@
+[cmdletbinding()]
+param(
+    [Parameter(Mandatory, Position = 0)]
+    [RestSharp.RestResponse]
+    $Response
+)
+
+if ($Response.Content.StartsWith("{") -and $Response.Content.EndsWith("}")) {
+    $content = $Response.Content | ConvertFrom-Json -Depth 50
+} else {
+    $content = $Response.Content
+}
+
+$requestStatus = [Thycotic.PowerShell.Common.RequestStatus]@{
+    StatusCode        = [int]$Response.StatusCode
+    StatusDescription = $Response.StatusDescription
+    IsSuccessful      = $Response.IsSuccessful
+    ResponseStatus    = $Response.ResponseStatus
+}
+New-Variable -Name tssLastResponse -Value $requestStatus -Description "Contains request status object for the command's last web request" -Visibility Public -Scope Global -Force
+
+if (-not $Response.IsSuccessful) {
+    $errorContent = $Response.Content
+    $PSCmdlet.WriteError([Management.Automation.ErrorRecord]::new([Exception]::new($errorContent),"ResultError", "NotSpecified", $invokeParams.Uri))
+} else {
+    return $content
+}
