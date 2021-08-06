26a39244c9d37a5d3b8f20f8c70c08df560899a7
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-08-06 15:14:32 -0500

    Get-TssSecretPolicy - new command

---
+++ src/Thycotic.SecretServer/classes/secret-policies/PolicyItem.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using Thycotic.PowerShell.Enums;
+
+namespace Thycotic.PowerShell.SecretPolicies
+{
+    public class PolicyItem
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public int ParentSecretPolicyItemId { get; set; }
+        public SecretPolicyApplyType PolicyApplyType { get; set; }
+        public int SecretPolicyItemId { get; set; }
+        public int SecretPolicyItemMapId { get; set; }
+        public string SectionName { get; set; }
+        public SshCommandMenuGroupMap[] SshCommandMenuGroupMaps { get; set; }
+        public UserGroupMap UserGroupMaps { get; set; }
+        public bool ValueBool { get; set; }
+        public int ValueInt { get; set; }
+        public int ValueSecretId { get; set; }
+        public string ValueString { get; set; }
+        public SecretPolicyType valueType { get; set; }
+    }
+}
