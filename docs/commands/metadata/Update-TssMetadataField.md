9e1603e5e6473d16a4289791bf99499d1785cbd9
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-08-17 07:42:32 -0500

    Update-TssMetadataField - new command

---
+++ docs/commands/metadata/Update-TssMetadataField.md
@@ -0,0 +1,179 @@
+# Update-TssMetadataField
+
+## SYNOPSIS
+Update metadata field for an item
+
+## SYNTAX
+
+```
+Update-TssMetadataField [-TssSession] <Session> -ItemId <Int32> -ItemType <MetadataType> -ItemDataId <Int32>
+ -FieldDataType <MetadataFieldDataType> -FieldValue <Object> [-WhatIf] [-Confirm] [<CommonParameters>]
+```
+
+## DESCRIPTION
+Update metadata field for an item
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+$session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+Search-TssMetadata -TssSession -ItemId 5 -Type User
+Update-TssMetadataField -TssSession $session -ItemId 5 -ItemType User -ItemDataId 5 -FieldValue 2
+```
+
+Review output from search command to identify the sequence ID of the metadata field (5 as ItemDataId for this example)
+Update the value to 2
+
+### EXAMPLE 2
+```
+$session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+Search-TssMetadata -TssSession -ItemId 5 -Type User
+Update-TssMetadataField -TssSession $session -ItemId 5 -ItemType User -ItemDataId 5 -FieldValue 2
+```
+
+Review output from search command to identify the sequence ID of the metadata field (5 as ItemDataId for this example)
+Update the value to 2
+
+## PARAMETERS
+
+### -TssSession
+TssSession object created by New-TssSession for authentication
+
+```yaml
+Type: Session
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -ItemId
+Item ID
+
+```yaml
+Type: Int32
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: Named
+Default value: 0
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ItemType
+Item Type
+
+```yaml
+Type: MetadataType
+Parameter Sets: (All)
+Aliases:
+Accepted values: Secret, User, Folder, Group
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ItemDataId
+Item Value
+
+```yaml
+Type: Int32
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: Named
+Default value: 0
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -FieldDataType
+Field Data Type
+
+```yaml
+Type: MetadataFieldDataType
+Parameter Sets: (All)
+Aliases:
+Accepted values: String, Boolean, Number, DateTime, User
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -FieldValue
+Item Value
+
+```yaml
+Type: Object
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -WhatIf
+Shows what would happen if the cmdlet runs.
+The cmdlet is not run.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: wi
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Confirm
+Prompts you for confirmation before running the cmdlet.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: cf
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+## OUTPUTS
+
+### Thycotic.PowerShell.Folders.Folder
+## NOTES
+Requires TssSession object returned by New-TssSession
+
+## RELATED LINKS
+
+[https://thycotic-ps.github.io/thycotic.secretserver/commands/folders/Update-TssMetadataField](https://thycotic-ps.github.io/thycotic.secretserver/commands/folders/Update-TssMetadataField)
+
+[https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/folders/Update-TssMetadataField.ps1](https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/folders/Update-TssMetadataField.ps1)
+
---
+++ src/functions/metadata/Update-TssMetadataField.ps1
@@ -0,0 +1,143 @@
+function Update-TssMetadataField {
+    <#
+    .SYNOPSIS
+    Update metadata field for an item
+
+    .DESCRIPTION
+    Update metadata field for an item
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Search-TssMetadata -TssSession -ItemId 5 -Type User
+    Update-TssMetadataField -TssSession $session -ItemId 5 -ItemType User -ItemDataId 5 -FieldValue 2
+
+    Review output from search command to identify the sequence ID of the metadata field (5 as ItemDataId for this example)
+    Update the value to 2
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Search-TssMetadata -TssSession -ItemId 5 -Type User
+    Update-TssMetadataField -TssSession $session -ItemId 5 -ItemType User -ItemDataId 5 -FieldValue 2
+
+    Review output from search command to identify the sequence ID of the metadata field (5 as ItemDataId for this example)
+    Update the value to 2
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/folders/Update-TssMetadataField
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/folders/Update-TssMetadataField.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [OutputType('Thycotic.PowerShell.Folders.Folder')]
+    [cmdletbinding(SupportsShouldProcess)]
+    param(
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession,
+
+        # Item ID
+        [Parameter(Mandatory)]
+        [int]
+        $ItemId,
+
+        # Item Type
+        [Parameter(Mandatory)]
+        [Thycotic.PowerShell.Enums.MetadataType]
+        $ItemType,
+
+        # Item Value
+        [Parameter(Mandatory)]
+        [int]
+        $ItemDataId,
+
+        # Field Data Type
+        [Parameter(Mandatory)]
+        [Thycotic.PowerShell.Enums.MetadataFieldDataType]
+        $FieldDataType,
+
+        # Item Value
+        [Parameter(Mandatory)]
+        [object]
+        $FieldValue
+    )
+    begin {
+        $updateParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeApiParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($updateParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000064' $PSCmdlet.MyInvocation
+            $uri = $TssSession.ApiUrl, 'metadata', $ItemType, $ItemId -join '/'
+            $invokeParams.Uri = $uri
+            $invokeParams.Method = 'PATCH'
+
+            $updateBody = @{data = @{} }
+            switch ($updateParams.Keys) {
+                'ItemDataId' { $updateBody.data.Add('metadataItemDataId', $ItemDataId) }
+                'FieldValue' {
+                    switch ($FieldDataType) {
+                        'String' {
+                            $stringValue = @{
+                                dirty = $true
+                                value = $FieldValue
+                            }
+                            $updateBody.data.Add('valueString', $stringValue)
+                        }
+                        'Boolean' {
+                            $bitValue = @{
+                                dirty = $true
+                                value = [boolean]$FieldValue
+                            }
+                            $updateBody.data.Add('valueBit', $bitValue)
+                        }
+                        'Number' {
+                            $numValue = @{
+                                dirty = $true
+                                value = $FieldValue
+                            }
+                            $updateBody.data.Add('valueNumber', $numValue)
+                        }
+                        'DateTime' {
+                            $dtValue = @{
+                                dirty = $true
+                                value = $FieldValue
+                            }
+                            $updateBody.data.Add('valueDateTime', $dtValue)
+                        }
+                        'User' {
+                            $userValue = @{
+                                dirty = $true
+                                value = $FieldValue
+                            }
+                            $updateBody.data.Add('valueInt', $userValue)
+                        }
+                    }
+                }
+            }
+
+            $invokeParams.Body = $updateBody | ConvertTo-Json -Depth 50
+            if ($PSCmdlet.ShouldProcess("Item ID: $ItemId", "$($invokeParams.Method) $uri with: `n$($invokeParams.Body)")) {
+                Write-Verbose "Performing the operation $($invokeParams.Method) $uri with: `n$($invokeParams.Body)"
+                try {
+                    $apiResponse = Invoke-TssApi @invokeParams
+                    $restResponse = . $ProcessResponse $apiResponse
+                } catch {
+                    Write-Warning "Issue updating metadata field on Item ID [$ItemId]"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+
+                if ($restResponse) {
+                    [Thycotic.PowerShell.Metadata.Field]$restResponse
+                }
+            }
+        } else {
+            Write-Warning 'No valid session found'
+        }
+    }
+}
