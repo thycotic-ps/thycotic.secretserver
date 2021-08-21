d54f62f83f5637e2f21c5cb3957092fb650a638a
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-08-21 15:04:13 -0500

    Get-TssConfigurationRpc - new command

---
+++ src/functions/configurations/Get-TssConfigurationRpc.ps1
@@ -0,0 +1,61 @@
+function Get-TssConfigurationRpc {
+    <#
+    .SYNOPSIS
+    Get Remote Password Change configuration
+
+    .DESCRIPTION
+    Get Remote Password Change configuration
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Get-TssConfigurationRpc -TssSession $session
+
+    Returns Remote Password Change configuration for Secret Server
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/configurations/Get-TssConfigurationRpc
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/configurations/Get-TssConfigurationRpc.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('Thycotic.PowerShell.Configuration.Rpc')]
+    param (
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeApiParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000064' $PSCmdlet.MyInvocation
+            $uri = $TssSession.ApiUrl, 'configuration', 'rpc' -join '/'
+            $invokeParams.Uri = $uri
+            $invokeParams.Method = 'GET'
+
+            Write-Verbose "Performing the operation $($invokeParams.Method) $($invokeParams.Uri)"
+            try {
+                $apiResponse = Invoke-TssApi @invokeParams
+                $restResponse = . $ProcessResponse $apiResponse
+            } catch {
+                Write-Warning "Issue getting RPC configuration"
+                $err = $_
+                . $ErrorHandling $err
+            }
+
+            if ($restResponse) {
+                [Thycotic.PowerShell.Configuration.Rpc]$restResponse
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
