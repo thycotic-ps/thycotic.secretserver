5649af615d4d795a3d14e896ab043125031c4e15
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-08-22 23:23:22 -0500

    Get-TssConfigurationAutoExport - new command

---
+++ docs/commands/configurations/Get-TssConfigurationAutoExport.md
@@ -0,0 +1,58 @@
+# Get-TssConfigurationAutoExport
+
+## SYNOPSIS
+Get the Automatic Export configuration
+
+## SYNTAX
+
+```
+Get-TssConfigurationAutoExport [-TssSession] <Session> [<CommonParameters>]
+```
+
+## DESCRIPTION
+Get the Automatic Export configuration
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+$session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+Get-TssConfigurationAutomaticExport -TssSession $session -Id 42
+```
+
+Return the Automatic Export configuration settings
+
+## PARAMETERS
+
+### -TssSession
+TssSession object created by New-TssSession for authentication
+
+```yaml
+Type: Session
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+## OUTPUTS
+
+### Thycotic.PowerShell.Configuration.AutomaticExport
+## NOTES
+Requires TssSession object returned by New-TssSession
+
+## RELATED LINKS
+
+[https://thycotic-ps.github.io/thycotic.secretserver/commands/configurations/Get-TssConfigurationAutoExport](https://thycotic-ps.github.io/thycotic.secretserver/commands/configurations/Get-TssConfigurationAutoExport)
+
+[https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/configurations/Get-TssConfigurationAutoExport.ps1](https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/configurations/Get-TssConfigurationAutoExport.ps1)
+
---
+++ src/functions/configurations/Get-TssConfigurationAutoExport.ps1
@@ -0,0 +1,62 @@
+function Get-TssConfigurationAutoExport {
+    <#
+    .SYNOPSIS
+    Get the Automatic Export configuration
+
+    .DESCRIPTION
+    Get the Automatic Export configuration
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Get-TssConfigurationAutomaticExport -TssSession $session -Id 42
+
+    Return the Automatic Export configuration settings
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/configurations/Get-TssConfigurationAutoExport
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/configurations/Get-TssConfigurationAutoExport.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('Thycotic.PowerShell.Configuration.AutomaticExport')]
+    param (
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeApiParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '11.0.000005' $PSCmdlet.MyInvocation
+            $restResponse = $null
+            $uri = $TssSession.ApiUrl, 'configuration', 'auto-export' -join '/'
+            $invokeParams.Uri = $uri
+            $invokeParams.Method = 'GET'
+
+            Write-Verbose "Performing the operation $($invokeParams.Method) $($invokeParams.Uri)"
+            try {
+                $apiResponse = Invoke-TssApi @invokeParams
+                $restResponse = . $ProcessResponse $apiResponse
+            } catch {
+                Write-Warning "Issue getting Automatic Export configuration"
+                $err = $_
+                . $ErrorHandling $err
+            }
+
+            if ($restResponse) {
+                [Thycotic.PowerShell.Configuration.AutomaticExport]$restResponse
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
