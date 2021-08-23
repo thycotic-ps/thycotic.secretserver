9d71f0fe78e77b16ce20ec9b6cac7c00d7cea3b0
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-08-22 21:37:33 -0500

    Get-TssConfigurationSecretIndexer - new command

---
+++ src/functions/configurations/Get-TssConfigurationSecretIndexer.ps1
@@ -0,0 +1,61 @@
+function Get-TssConfigurationSecretIndexer {
+    <#
+    .SYNOPSIS
+    Get Secret Indexer configuration
+
+    .DESCRIPTION
+    Get Secret Indexer configuration
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Get-TssConfigurationIndexer -TssSession $session - some test value
+
+    Return configuration of the Secret Search Indexer
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/configuration/Get-TssConfigurationSecretIndexer
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/configuration/Get-TssConfigurationSecretIndexer.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('Thycotic.PowerShell.Configuration.SearchIndexer')]
+    param (
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeApiParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000064' $PSCmdlet.MyInvocation
+                $uri = $TssSession.ApiUrl, 'configuration', 'secret-search-indexer' -join '/'
+                $invokeParams.Uri = $uri
+                $invokeParams.Method = 'GET'
+
+                Write-Verbose "Performing the operation $($invokeParams.Method) $($invokeParams.Uri)"
+                try {
+                    $apiResponse = Invoke-TssApi @invokeParams
+                    $restResponse = . $ProcessResponse $apiResponse
+                } catch {
+                    Write-Warning "Issue getting Search Indexer configuration"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+
+                if ($restResponse) {
+                    [Thycotic.PowerShell.Configuration.SearchIndexer]$restResponse
+                }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
