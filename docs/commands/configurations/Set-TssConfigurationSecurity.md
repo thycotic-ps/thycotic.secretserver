8c0c4e81371d252e545e227cfb5bde1c4e5692d0
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-09-02 07:01:45 -0500

    Set-TssConfigurationSecurity - new command

---
+++ docs/commands/configurations/Set-TssConfigurationSecurity.md
@@ -0,0 +1,294 @@
+# Set-TssConfigurationSecurity
+
+## SYNOPSIS
+Set Security Configuration
+
+## SYNTAX
+
+```
+Set-TssConfigurationSecurity [-TssSession] <Session> [-AuditTlsErrors] [-AuditTlsErrorsDebug]
+ [-CertificatePolicyOption <String>] [-CertificateId <String>] [-FileRestriction] [-FileExtension <String>]
+ [-MaxFileSize <String>] [-MaxFileSizeSupported] [-FrameBlocking] [-Fips] [-ForceHttps] [-HideVersion]
+ [-WpfRequireDomainMatch] [-WhatIf] [-Confirm] [<CommonParameters>]
+```
+
+## DESCRIPTION
+Set Security Configuration
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+$session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+Set-TssConfigurationSecurity -TssSession $session -ForceHttps
+```
+
+Enable Force HTTPS
+
+### EXAMPLE 2
+```
+$session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+Set-TssConfigurationSecurity -TssSession $session -ForceHttps:$false
+```
+
+Disabling Force HTTPS
+
+## PARAMETERS
+
+### -TssSession
+TssSession object created by New-TssSession for authentication
+
+```yaml
+Type: Session
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -AuditTlsErrors
+Apply TLS Certificate Chain Policy and Error Auditing
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -AuditTlsErrorsDebug
+Enable TLS Debugging and connection tracking
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -CertificatePolicyOption
+Certificate chain policy options
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -CertificateId
+Client Certificate ID
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -FileRestriction
+Enable file restrictions
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -FileExtension
+File extensions to restrict
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -MaxFileSize
+Max file size in bytes
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -MaxFileSizeSupported
+Max allowed by ASP.NET
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -FrameBlocking
+Enable frame blocking
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Fips
+Enable FIPS compliance
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ForceHttps
+Enable Force HTTPS
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -HideVersion
+Hide Version number
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -WpfRequireDomainMatch
+Web Password Filler (WPF) requires full domain match
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -WhatIf
+Shows what would happen if the cmdlet runs.
+The cmdlet is not run.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: wi
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Confirm
+Prompts you for confirmation before running the cmdlet.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: cf
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+## OUTPUTS
+
+## NOTES
+Requires TssSession object returned by New-TssSession
+
+## RELATED LINKS
+
+[https://thycotic-ps.github.io/thycotic.secretserver/commands/Folder name/Set-TssConfigurationSecurity](https://thycotic-ps.github.io/thycotic.secretserver/commands/Folder name/Set-TssConfigurationSecurity)
+
+[https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/Folder name/Set-TssConfigurationSecurity.ps1](https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/Folder name/Set-TssConfigurationSecurity.ps1)
+
---
+++ src/functions/configurations/Set-TssConfigurationSecurity.ps1
@@ -0,0 +1,208 @@
+function Set-TssConfigurationSecurity {
+    <#
+    .SYNOPSIS
+    Set Security Configuration
+
+    .DESCRIPTION
+    Set Security Configuration
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Set-TssConfigurationSecurity -TssSession $session -ForceHttps
+
+    Enable Force HTTPS
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Set-TssConfigurationSecurity -TssSession $session -ForceHttps:$false
+
+    Disabling Force HTTPS
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/Folder name/Set-TssConfigurationSecurity
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/Folder name/Set-TssConfigurationSecurity.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [cmdletbinding(SupportsShouldProcess, DefaultParameterSetName = 'all')]
+    param(
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession,
+
+        # Apply TLS Certificate Chain Policy and Error Auditing
+        [switch]
+        $AuditTlsErrors,
+
+        # Enable TLS Debugging and connection tracking
+        [switch]
+        $AuditTlsErrorsDebug,
+
+        # Certificate chain policy options
+        [string]
+        $CertificatePolicyOption,
+
+        # Client Certificate ID
+        [string]
+        $CertificateId,
+
+        # Enable file restrictions
+        [switch]
+        $FileRestriction,
+
+        # File extensions to restrict
+        [string]
+        $FileExtension,
+
+        # Max file size in bytes
+        [string]
+        $FileMaxSize,
+
+        # Max allowed by ASP.NET
+        [switch]
+        $FileMaxSizeSupported,
+
+        # Enable frame blocking
+        [switch]
+        $FrameBlocking,
+
+        # Enable FIPS compliance
+        [switch]
+        $Fips,
+
+        # Enable Force HTTPS
+        [switch]
+        $ForceHttps,
+
+        # Hide Version number
+        [switch]
+        $HideVersion,
+
+        # Web Password Filler (WPF) requires full domain match
+        [switch]
+        $WpfRequireDomainMatch
+    )
+    begin {
+        $setParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeApiParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($setParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '11.0.000005' $PSCmdlet.MyInvocation
+            $uri = $TssSession.ApiUrl, 'configuration', 'security' -join '/'
+            $invokeParams.Uri = $uri
+            $invokeParams.Method = 'PATCH'
+
+            $setBody = @{ data = @{ } }
+            switch ($setParams.Keys) {
+                'AuditTlsErrors' {
+                    $auditTls = @{
+                        dirty = $true
+                        value = [boolean]$AuditTls
+                    }
+                    $setBody.data.Add('auditTlsErrors',$auditTls)
+                }
+                'AuditTlsErrorsDebug' {
+                    $auditTlsErrorDebug = @{
+                        dirty = $true
+                        value = [boolean]$AuditTlsErrorsDebug
+                    }
+                    $setBody.data.Add('auditTlsErrorsDebug',$auditTlsErrorDebug)
+                }
+                'CertificatePolicyOption' {
+                    $certOption = @{
+                        dirty = $true
+                        value = $CertificatePolicyOption
+                    }
+                    $setBody.data.Add('certificateChainPolicyOptions',$certOption)
+                }
+                'CertificateId' {
+                    $certId = @{
+                        dirty = $true
+                        value = $CertificateId
+                    }
+                    $setBody.data.Add('clientCertificateIds',$certId)
+                }
+                'FileRestriction' {
+                    $fileRestrict = @{
+                        dirty = $true
+                        value = [boolean]$FileRestriction
+                    }
+                    $setBody.data.Add('enableFileRestrictions',$fileRestrict)
+                    if ($setParams.ContainsKey('FileExtension')) {
+                        $fileExt = @{
+                            dirty = $true
+                            value = $FileExtension
+                        }
+                        $setBody.data.Add('fileExtensionRestrictions',$fileExt)
+                    }
+                }
+                'FileMaxSize' {
+                    $maxFile = @{
+                        dirty = $true
+                        value = $FileMaxSize
+                    }
+                    $setBody.data.Add('maximumFileSizeBytes',$maxFile)
+                }
+                'FileMaxSizeSupported' {
+                    $maxSupported = @{
+                        dirty = $true
+                        value = [boolean]$FileMaxSizeSupported
+                    }
+                    $setBody.data.Add('maximumFileSizeSupported',$maxSupported)
+                }
+                'Fips' {
+                    $enableFips = @{
+                        dirty = $true
+                        value = [boolean]$Fips
+                    }
+                    $setBody.data.Add('fipsEnabled',$enableFips)
+                }
+                'ForceHttps' {
+                    $httpsEnforce = @{
+                        dirty = $true
+                        value = [boolean]$ForceHttps
+                    }
+                    $setBody.data.Add('forceHttps',$httpsEnforce)
+                }
+                'HideVersion' {
+                    $hideVer = @{
+                        dirty = $true
+                        value = [boolean]$HideVersion
+                    }
+                    $setBody.data.Add('hideVersionNumber',$hideVer)
+                }
+                'WpfRequireDomainMatch' {
+                    $wpfDomain = @{
+                        dirty = $true
+                        value = [boolean]$WpfRequireDomainMatch
+                    }
+                    $setBody.data.Add('webPasswordFillerRequiresFullDomainMatch',$wpfDomain)
+                }
+            }
+
+            $invokeParams.Body = $setBody | ConvertTo-Json -Depth 100
+            if ($PSCmdlet.ShouldProcess("Security Configuration", "$($invokeParams.Method) $($invokeParams.Uri) with:`n$($invokeParams.Body)`n")) {
+                Write-Verbose "Performing the operation $($invokeParams.Method) $($invokeParams.Uri) with:`n$($invokeParams.Body)`n"
+                try {
+                    $apiResponse = Invoke-TssApi @invokeParams
+                    $restResponse = . $ProcessResponse $apiResponse
+                } catch {
+                    Write-Warning "Issue setting configuration"
+                    $err = $_
+                    . $ErrorHandling $err
+                }
+            }
+            if ($restResponse) {
+                Write-Verbose "Setting updated successfully"
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
