aa34f656c2ed5716b37880031d3af2cde4f02c5c
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-09-28 01:15:24 -0500

    Search-TssIpRestriction - new command to search IP Address Restrictions

---
+++ docs/commands/ipaddress-restrictions/Search-TssIpRestriction.md
@@ -0,0 +1,73 @@
+# Search-TssIpRestriction
+
+## SYNOPSIS
+Search for IP Address restrictions
+
+## SYNTAX
+
+```
+Search-TssIpRestriction [-TssSession] <Session> [-SortBy <String>] [<CommonParameters>]
+```
+
+## DESCRIPTION
+Search for IP Address restrictions
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+$session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+Search-TssIpRestriction -TssSession $session
+```
+
+Return list of IP Address restrictions for Secret Server
+
+## PARAMETERS
+
+### -TssSession
+TssSession object created by New-TssSession for authentication
+
+```yaml
+Type: Session
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -SortBy
+Sort by specific property, default Id
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: Id
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+## OUTPUTS
+
+### Thycotic.PowerShell.IpRestrictions.Summary
+## NOTES
+Requires TssSession object returned by New-TssSession
+
+## RELATED LINKS
+
+[https://thycotic-ps.github.io/thycotic.secretserver/commands/ipaddress-restrictions/Search-TssIpRestriction](https://thycotic-ps.github.io/thycotic.secretserver/commands/ipaddress-restrictions/Search-TssIpRestriction)
+
+[https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/ipaddress-restrictions/Search-TssIpRestriction.ps1](https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/ipaddress-restrictions/Search-TssIpRestriction.ps1)
+
---
+++ src/functions/ipaddress-restrictions/Search-TssIpRestriction.ps1
@@ -0,0 +1,70 @@
+function Search-TssIpRestriction {
+    <#
+    .SYNOPSIS
+    Search for IP Address restrictions
+
+    .DESCRIPTION
+    Search for IP Address restrictions
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/ipaddress-restrictions/Search-TssIpRestriction
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/ipaddress-restrictions/Search-TssIpRestriction.ps1
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Search-TssIpRestriction -TssSession $session
+
+    Return list of IP Address restrictions for Secret Server
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('Thycotic.PowerShell.IpRestrictions.Summary')]
+    param (
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession,
+
+        # Sort by specific property, default Id
+        [string]
+        $SortBy = 'Id'
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeApiParams $TssSession
+    }
+    process {
+        Write-Verbose "Provided command parameters: $(. $GetInvocation $PSCmdlet.MyInvocation)"
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            . $CheckVersion $TssSession '10.9.000064' $PSCmdlet.MyInvocation
+            $uri = $TssSession.ApiUrl, 'ipaddress-restrictions' -join '/'
+            $uri = $uri, "sortBy[0].direction=asc&sortBy[0].name=$SortBy&take=$($TssSession.Take)" -join '?'
+            $invokeParams.Method = 'GET'
+
+            $invokeParams.Uri = $uri
+
+            Write-Verbose "Performing the operation $($invokeParams.Method) $($invokeParams.Uri)"
+            try {
+                $apiResponse = Invoke-TssApi @invokeParams
+                $restResponse = . $ProcessResponse $apiResponse
+            } catch {
+                Write-Warning "Issue on search request"
+                $err = $_
+                . $ErrorHandling $err
+            }
+
+            if ($restResponse.records.Count -le 0 -and $restResponse.records.Length -eq 0) {
+                Write-Warning "No records found"
+            }
+            if ($restResponse.records) {
+                [Thycotic.PowerShell.IpRestrictions.Summary[]]$restResponse.records
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
