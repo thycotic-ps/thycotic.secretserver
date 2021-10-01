3289f9096a5513ca401a3e7e75574ef14b11645c
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-10-01 11:45:47 -0500

    Template classes - added enumerators

--- src/Thycotic.SecretServer/classes/secret-templates/Field.cs
+++ src/Thycotic.SecretServer/classes/secret-templates/Field.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using Thycotic.PowerShell.Enums;
 
 namespace Thycotic.PowerShell.SecretTemplates
 {
@@ -10,7 +11,7 @@ namespace Thycotic.PowerShell.SecretTemplates
         public string Description { get; set; }
         public string DisplayName { get; set; }
         public int EditablePermission { get; set; }
-        public string EditRequires { get; set; }
+        public TemplateEditRequiresOptions EditRequires { get; set; }
         public string FieldSlugName { get; set; }
         public string GeneratePasswordCharacterSet { get; set; }
         public int GeneratePasswordLength { get; set; }
@@ -24,7 +25,7 @@ namespace Thycotic.PowerShell.SecretTemplates
         public bool IsRequired { get; set; }
         public bool IsUrl { get; set; }
         public bool IsList { get; set; }
-        public string ListType { get; set; }
+        public TemplateFieldListType ListType { get; set; }
         public bool MustEncrypt { get; set; }
         public string Name { get; set; }
         public int PasswordRequirementId { get; set; }
