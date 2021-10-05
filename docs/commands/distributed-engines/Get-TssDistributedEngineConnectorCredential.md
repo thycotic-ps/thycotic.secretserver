67c14d60a9537e697dd0fd74cb2487af27a823a6
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2021-10-05 12:21:08 -0500

    Get-TssDistributeEngineConnectorCredential - new command to pull username/password for the connector

---
+++ docs/commands/distributed-engines/Get-TssDistributedEngineConnectorCredential.md
@@ -0,0 +1,73 @@
+# Get-TssDistributedEngineConnectorCredential
+
+## SYNOPSIS
+Get the Site Connector credential
+
+## SYNTAX
+
+```
+Get-TssDistributedEngineConnectorCredential [-TssSession] <Session> -SiteConnectorId <Int32>
+ [<CommonParameters>]
+```
+
+## DESCRIPTION
+Get the Site Connector credential
+
+## EXAMPLES
+
+### EXAMPLE 1
+```
+$session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+Get-TssDistributedEngineConnectorCredential -TssSession $session -SiteConnectorId 5
+```
+
+Return the username and password for the Site Connector ID 5
+
+## PARAMETERS
+
+### -TssSession
+TssSession object created by New-TssSession for authentication
+
+```yaml
+Type: Session
+Parameter Sets: (All)
+Aliases:
+
+Required: True
+Position: 1
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -SiteConnectorId
+Short description for parameter
+
+```yaml
+Type: Int32
+Parameter Sets: (All)
+Aliases: Id
+
+Required: True
+Position: Named
+Default value: 0
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+## OUTPUTS
+
+## NOTES
+Requires TssSession object returned by New-TssSession
+
+## RELATED LINKS
+
+[https://thycotic-ps.github.io/thycotic.secretserver/commands/distributed-engines/Get-TssDistributedEngineConnectorCredential](https://thycotic-ps.github.io/thycotic.secretserver/commands/distributed-engines/Get-TssDistributedEngineConnectorCredential)
+
+[https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/distributed-engines/Get-TssDistributedEngineConnectorCredential.ps1](https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/distributed-engines/Get-TssDistributedEngineConnectorCredential.ps1)
+
---
+++ src/functions/distributed-engines/Get-TssDistributedEngineConnectorCredential.ps1
@@ -0,0 +1,67 @@
+function Get-TssDistributedEngineConnectorCredential {
+    <#
+    .SYNOPSIS
+    Get the Site Connector credential
+
+    .DESCRIPTION
+    Get the Site Connector credential
+
+    .EXAMPLE
+    $session = New-TssSession -SecretServer https://alpha -Credential $ssCred
+    Get-TssDistributedEngineConnectorCredential -TssSession $session -SiteConnectorId 5
+
+    Return the username and password for the Site Connector ID 5
+
+    .LINK
+    https://thycotic-ps.github.io/thycotic.secretserver/commands/distributed-engines/Get-TssDistributedEngineConnectorCredential
+
+    .LINK
+    https://github.com/thycotic-ps/thycotic.secretserver/blob/main/src/functions/distributed-engines/Get-TssDistributedEngineConnectorCredential.ps1
+
+    .NOTES
+    Requires TssSession object returned by New-TssSession
+    #>
+    [CmdletBinding()]
+    [OutputType('')]
+    param (
+        # TssSession object created by New-TssSession for authentication
+        [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
+        [Thycotic.PowerShell.Authentication.Session]
+        $TssSession,
+
+        # Short description for parameter
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias("Id")]
+        [int]
+        $SiteConnectorId
+    )
+    begin {
+        $tssParams = $PSBoundParameters
+        $invokeParams = . $GetInvokeApiParams $TssSession
+    }
+    process {
+        Get-TssInvocation $PSCmdlet.MyInvocation
+        if ($tssParams.ContainsKey('TssSession') -and $TssSession.IsValidSession()) {
+            Compare-TssVersion $TssSession '10.9.000064' $PSCmdlet.MyInvocation
+            $uri = $TssSession.ApiUrl, 'distributed-engine', 'site-connector', $SiteConnectorId, 'credentials' -join '/'
+            $invokeParams.Uri = $uri
+            $invokeParams.Method = 'GET'
+
+            Write-Verbose "Performing the operation $($invokeParams.Method) $($invokeParams.Uri)"
+            try {
+                $apiResponse = Invoke-TssApi @invokeParams
+                $restResponse = . $ProcessResponse $apiResponse
+            } catch {
+                Write-Warning "Issue getting reference name on [$var name]"
+                $err = $_
+                . $ErrorHandling $err
+            }
+
+            if ($restResponse) {
+                $restResponse
+            }
+        } else {
+            Write-Warning "No valid session found"
+        }
+    }
+}
