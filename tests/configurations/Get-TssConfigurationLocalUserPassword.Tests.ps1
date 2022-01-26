dedda223b9cf1db14128ae66f17d3b8dd54a18ab
Ricky White <ricky@migusgroup.com>
2022-01-26 16:53:32 -0500

    Fixed test compilation errors

--- tests/configurations/Get-TssConfigurationLocalUserPassword.Tests.ps1
+++ tests/configurations/Get-TssConfigurationLocalUserPassword.Tests.ps1
@@ -3,7 +3,7 @@ BeforeDiscovery {
 }
 Describe "$commandName verify parameters" {
     BeforeDiscovery {
-        [object[]]$knownParameters = 'TssSession',
+        [object[]]$knownParameters = [String]'TssSession'
         [object[]]$currentParams = ([Management.Automation.CommandMetaData]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')).Parameters.Keys
         [object[]]$commandDetails = [System.Management.Automation.CommandInfo]$ExecutionContext.SessionState.InvokeCommand.GetCommand($commandName,'Function')
         $unknownParameters = Compare-Object -ReferenceObject $knownParameters -DifferenceObject $currentParams -PassThru
