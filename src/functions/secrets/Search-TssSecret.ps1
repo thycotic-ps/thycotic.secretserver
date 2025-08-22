4a02d030902123b9af4f9313026b986cc050019b
TiiimK_ <46269545+TiiimK@users.noreply.github.com>
2025-08-22 17:01:51 +0200

    Update Search-TssSecret.ps1
    Workaround for bug #399

--- src/functions/secrets/Search-TssSecret.ps1
+++ src/functions/secrets/Search-TssSecret.ps1
@@ -271,7 +271,11 @@ function Search-TssSecret {
             }
 
             if ($restResponse.records) {
-                [Thycotic.PowerShell.Secrets.Summary[]]$restResponse.records
+                $restResponse | ForEach-Object {
+                    $NonEmptyProperties = $_.restResponse.Properties | Where-Object {$_.Value} | Select-Object -ExpandProperty Name
+                    ($_ | Select-Object -Property $NonEmptyProperties).records
+                }
+                [Thycotic.PowerShell.Secrets.Summary[]]$NonEmptyProperties.records
             }
         } else {
             Write-Warning 'No valid session found'
